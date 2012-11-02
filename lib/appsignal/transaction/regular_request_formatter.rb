3dac1ea30b53407eed2e379692a2464c58f9e617
Steven Weller <github@suweller.com>
2012-11-02 15:44:13 +0100

    Prevent regular requests from dumping lots of data

--- lib/appsignal/transaction/regular_request_formatter.rb
+++ lib/appsignal/transaction/regular_request_formatter.rb
@@ -2,6 +2,10 @@ module Appsignal
   class TransactionFormatter
     class RegularRequestFormatter < Appsignal::TransactionFormatter
 
+      def sanitized_event_payload(*args)
+        {}
+      end
+
     end
   end
 end
--- spec/appsignal/transaction/regular_request_formatter_spec.rb
+++ spec/appsignal/transaction/regular_request_formatter_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Appsignal::TransactionFormatter::RegularRequestFormatter do
+  let(:parent) { Appsignal::TransactionFormatter }
+  let(:transaction) { appsignal_transaction }
+  let(:klass) { parent::RegularRequestFormatter }
+  let(:regular) { klass.new(transaction) }
 
+  describe "#sanitized_event_payload" do
+    subject { regular.sanitized_event_payload(:whatever, :arguments) }
+
+    it { should == {} }
+  end
 end
