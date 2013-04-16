832ed262e8b3b8e9821532f4073fb6743772f38a
Steven Weller <github@suweller.com>
2013-04-16 11:34:57 -0400

    Check for duplicate api-keys and refactor #load

--- spec/appsignal/config_spec.rb
+++ spec/appsignal/config_spec.rb
@@ -2,63 +2,114 @@ require 'spec_helper'
 
 describe Appsignal::Config do
   let(:logger_parameter) { [] }
-  subject { Appsignal::Config.new(Dir.pwd, 'test', *logger_parameter).load }
+  let(:path) { Dir.pwd }
+  let(:config) { Appsignal::Config.new(path, 'test', *logger_parameter) }
 
-  it {
-    should == {
-      :ignore_exceptions => [],
-      :endpoint => 'http://localhost:3000/1',
-      :slow_request_threshold => 200,
-      :api_key => 'ghi',
-      :active => true
+  describe "#load" do
+    subject { config.load }
+
+    it "should never have logged an error" do
+      Appsignal.logger.should_not_receive(:error)
+      subject
+    end
+
+    it {
+      should == {
+        :ignore_exceptions => [],
+        :endpoint => 'http://localhost:3000/1',
+        :slow_request_threshold => 200,
+        :api_key => 'ghi',
+        :active => true
+      }
     }
-  }
 
-  context 'when there is no config file' do
-    before { Dir.stub(:pwd => '/not/existing') }
+    context 'when there is no config file' do
+      before { Dir.stub(:pwd => '/not/existing') }
 
-    it "should log error" do
-      Appsignal.logger.should_receive(:error).with(
-        "config not found at:"\
-        " /not/existing/config/appsignal.yml"
-      )
+      it { should be_nil }
     end
 
-    context "when the logger has no #error method" do
-      let(:logger) { mock(:logger) }
-      let(:logger_parameter) { [logger] }
+    context "the env is not in the config" do
+      before { config.stub(:current_environment_present => false) }
 
-      it "should log the error using #important" do
-        logger.should_receive(:important).with(
-          "config not found at:"\
-          " /not/existing/config/appsignal.yml"
-        )
-      end
+      it { should be_nil }
     end
 
-    after { subject }
+    context "when an api key is used for more then one environment" do
+      before { config.stub(:used_unique_api_keys => false) }
+
+      it { should be_nil }
+    end
   end
 
-  context "the env is not in the config" do
-    subject { Appsignal::Config.new(Dir.pwd, 'staging', *logger_parameter).load }
+  # protected
 
-    it "should generate error" do
-      Appsignal.logger.should_receive(:error).with(
-        "config for 'staging' not found"
-      )
+  describe "#load_configurations_from_disk" do
+    subject do
+      config.send(:load_configurations_from_disk)
+      config.configurations
     end
 
-    context "when the logger has no #error method" do
-      let(:logger) { mock(:logger) }
-      let(:logger_parameter) { [logger] }
+    context "when the file is present" do
+      before { config.should_not_receive(:carefully_log_error) }
 
-      it "should log the error using #important" do
-        logger.should_receive(:important).with(
-          "config for 'staging' not found"
-        )
+      it { should_not be_empty }
+    end
+
+    context "when the file is not present" do
+      before do
+        config.should_receive(:carefully_log_error)
+        config.stub(:project_path => '/non/existing')
       end
+
+      it { should be_empty }
+    end
+  end
+
+  describe "#used_unique_api_keys" do
+    let(:env) { {:api_key => :foo} }
+    subject { config.send(:used_unique_api_keys) }
+
+    context "when using all unique keys" do
+      before do
+        config.should_not_receive(:carefully_log_error)
+        config.stub(:configurations => {1 => env})
+      end
+
+      it { should be_true }
     end
 
-    after { subject }
+    context "when using non-unique keys" do
+      before do
+        config.should_receive(:carefully_log_error).
+          with("Duplicate API keys found in appsignal.yml")
+        config.stub(:configurations => {:production => env, :staging => env})
+      end
+
+      it { should be_false }
+    end
+  end
+
+  describe "#current_environment_present" do
+    subject { config.send(:current_environment_present) }
+
+    context "when the current environment is present" do
+      before do
+        config.should_not_receive(:carefully_log_error)
+        config.stub(:configurations => {:test => :foo})
+      end
+
+      it { should be_true }
+    end
+
+    context "when the current environment is absent" do
+      before do
+        config.should_receive(:carefully_log_error).
+          with("config for 'test' not found")
+        config.stub(:configurations => {})
+      end
+
+      it { should be_false }
+    end
   end
 end
