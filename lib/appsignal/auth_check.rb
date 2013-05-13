957d19366bfc6a40901582d6116e065db893f6e4
Steven Weller <github@suweller.com>
2013-05-13 14:12:09 +0200

    Allow overwriting logger and config

--- lib/appsignal/auth_check.rb
+++ lib/appsignal/auth_check.rb
@@ -2,17 +2,24 @@ module Appsignal
   class AuthCheck
     ACTION = 'auth'.freeze
 
-    attr_reader :config
+    attr_reader :environment, :logger
     attr_accessor :transmitter
     delegate :uri, :to => :transmitter
 
-    def initialize(environment)
-      @config = Appsignal::Config.new(Rails.root, environment).load
+    def initialize(*args)
+      @environment = args.shift
+      options = args.empty? ? {} : args.last
+      @config = options[:config]
+      @logger = options[:logger]
+    end
+
+    def config
+      @config ||= Appsignal::Config.new(Rails.root, environment, logger).load
     end
 
     def perform
       self.transmitter = Appsignal::Transmitter.new(
-        @config[:endpoint], ACTION, @config[:api_key]
+        config[:endpoint], ACTION, config[:api_key]
       )
       transmitter.transmit({})
     end
