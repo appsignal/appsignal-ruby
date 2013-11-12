b73a88216f0454523244075de17d9f1be69259cf
Thijs Cadier <thijs@80beans.com>
2013-11-12 18:47:12 +0100

    Apsignal.start_logger

--- lib/appsignal/integrations/rails.rb
+++ lib/appsignal/integrations/rails.rb
@@ -10,16 +10,8 @@ if defined?(::Rails)
         end
 
         config.after_initialize do
-          # Setup logging
-          if File.writable?(Rails.root.join('log'))
-            output = Rails.root.join('log/appsignal.log')
-          else
-            output = STDOUT
-          end
-          Appsignal.logger = Logger.new(output).tap do |l|
-            l.level = Logger::INFO
-          end
-          Appsignal.flush_in_memory_log
+          # Start logger
+          Appsignal.start_logger(Rails.root.join('log'))
 
           # Load config
           Appsignal.config = Appsignal::Config.new(Rails.root, Rails.env)
