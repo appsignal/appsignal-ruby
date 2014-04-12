8065b3efc7a0673bfe9c89cc362f682cb0e7577d
Thijs Cadier <thijs@80beans.com>
2014-04-12 15:11:22 +0200

    Fix spec warning

--- spec/lib/appsignal_spec.rb
+++ spec/lib/appsignal_spec.rb
@@ -350,9 +350,8 @@ describe Appsignal do
       context "without agent" do
         let(:agent) { nil }
 
-        it "should not change paused state on agent" do
-          agent.should_not_receive(:paused=).with(true)
-          agent.should_not_receive(:paused=).with(false)
+        it "should not crash" do
+          # just execute the after block
         end
       end
 
