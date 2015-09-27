6b121189c4e732a6f4b6e471631e0c76c2768280
Thijs Cadier <thijs@appsignal.com>
2015-09-27 17:26:46 +0200

    Logging, checksum validation and error handling for agent installation

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,63 +1,85 @@
+require 'digest'
+require 'logger'
 require 'mkmf'
 require 'fileutils'
 require 'open-uri'
 require 'zlib'
 require 'rubygems/package'
+require 'yaml'
 require File.expand_path('../../lib/appsignal/version.rb', __FILE__)
 
-HOST            = 'd135dj0rjqvssy.cloudfront.net'
-SUPPORTED_ARCHS = %w(x86_64-linux x86_64-darwin)
-EXT_PATH        = File.expand_path('..', __FILE__)
-LIB_EXTENSION   = case Gem::Platform.local.os
-                  when /darwin/
-                    'dylib'
-                  else
-                    'so'
-                  end
+EXT_PATH     = File.expand_path('..', __FILE__)
+AGENT_CONFIG = YAML.load(File.read(File.join(EXT_PATH, 'agent.yml')))
+ARCH         = "#{Gem::Platform.local.cpu}-#{Gem::Platform.local.os}"
 
 def ext_path(path)
   File.join(EXT_PATH, path)
 end
 
+def logger
+  @logger ||= Logger.new(File.join(EXT_PATH, 'install.log'))
+end
+
 def installation_failed(reason)
-  puts "Installation failed: #{reason}"
+  logger.error "Installation failed: #{reason}"
   File.open(File.join(EXT_PATH, 'Makefile'), 'w') do |file|
       file.write "default:\nclean:\ninstall:"
   end
 end
 
-arch = "#{Gem::Platform.local.cpu}-#{Gem::Platform.local.os}"
+def install
+  logger.info "Installing appsignal agent for Ruby #{RUBY_VERSION} on #{RUBY_PLATFORM}"
 
-if SUPPORTED_ARCHS.include?(arch)
-  begin
-    if !File.exists?(ext_path('appsignal-agent')) || !File.exists?(ext_path("libappsignal.#{LIB_EXTENSION}"))
-      archive_url = "https://#{HOST}/#{Appsignal::AGENT_VERSION}/appsignal-agent-#{arch}.tar.gz"
-      archive     = open(archive_url)
-      Gem::Package::TarReader.new(Zlib::GzipReader.open(archive)) do |tar|
-        tar.each do |entry|
-          if entry.file?
-            File.open(ext_path(entry.full_name), 'wb') do |f|
-              f.write(entry.read)
-            end
+  unless AGENT_CONFIG[:triples].keys.include?(ARCH)
+    installation_failed(
+      "AppSignal currently does not support your system architecture (#{ARCH})." \
+      "Please let us know at support@appsignal.com, we aim to support everything our customers run."
+    )
+    return
+  end
+
+  arch_config = AGENT_CONFIG[:triples][ARCH]
+
+  unless File.exists?(ext_path('appsignal-agent')) &&
+           File.exists?(ext_path(arch_config[:lib_filename])) &&
+           File.exists?(ext_path('appsignal_extension.h'))
+    logger.info "Downloading agent release from #{arch_config[:download_url]}"
+
+    archive = open(arch_config[:download_url])
+
+    if Digest::SHA256.hexdigest(archive.read) == arch_config[:checksum]
+      logger.info 'Checksum of downloaded archive verified, extracting archive'
+    else
+      installation_failed(
+        "Aborting installation, checksum of downloaded archive could not be verified: " \
+        "Expected '#{arch_config[:checksum]}', got '#{checksum}'."
+      )
+      return
+    end
+
+    Gem::Package::TarReader.new(Zlib::GzipReader.open(archive)) do |tar|
+      tar.each do |entry|
+        if entry.file?
+          File.open(ext_path(entry.full_name), 'wb') do |f|
+            f.write(entry.read)
           end
         end
       end
-      FileUtils.chmod(0755, ext_path('appsignal-agent'))
     end
-
-    if find_library('appsignal', 'appsignal_start', EXT_PATH) &&
-         find_executable('appsignal-agent', EXT_PATH) &&
-         find_header('appsignal_extension.h', EXT_PATH)
-      create_makefile 'appsignal_extension'
-    else
-      installation_failed("Extension files were not present")
-    end
-  rescue => ex
-      installation_failed("Exception while installing: #{ex}")
+    FileUtils.chmod(0755, ext_path('appsignal-agent'))
   end
-else
-  installation_failed(
-    "AppSignal currently does not support your system architecture (#{arch})." \
-    "Please let us know at support@appsignal.com, we aim to support everything our customers run."
-  )
+
+  logger.info "Creating makefile"
+  if find_library('appsignal', 'appsignal_start', EXT_PATH) &&
+       find_executable('appsignal-agent', EXT_PATH) &&
+       find_header('appsignal_extension.h', EXT_PATH)
+    create_makefile 'appsignal_extension'
+    logger.info 'Successfully installed appsignal extension'
+  else
+    installation_failed "Aborting installation, extension files were not present"
+  end
+rescue => ex
+  installation_failed "Exception while installing: #{ex}"
 end
+
+install
