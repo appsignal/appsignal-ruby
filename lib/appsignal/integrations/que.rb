8bbcad56925e7ce162fc43ca5fbcd673ce0db4e9
wvengen <willem@thequestionmark.org>
2015-10-02 13:58:56 +0200

    Support Ruby 1.9.3 again with Que integration

--- lib/appsignal/integrations/que.rb
+++ lib/appsignal/integrations/que.rb
@@ -4,26 +4,36 @@ if defined?(::Que)
   module Appsignal
     module Integrations
       module QuePlugin
-        def _run
-          Appsignal.monitor_transaction(
-            'perform_job.que',
-            :class    => attrs[:job_class],
-            :method   => 'run',
-            :metadata => {
-              :id        => attrs[:job_id],
-              :queue     => attrs[:queue],
-              :run_at    => attrs[:run_at].to_s,
-              :priority  => attrs[:priority],
-              :attempts  => attrs[:error_count].to_i
-            },
-            :params   => attrs[:args]
-          ) { super }
+        def self.included(base)
+          base.class_eval do
+
+            def _run_with_appsignal
+              Appsignal.monitor_transaction(
+                'perform_job.que',
+                :class    => attrs[:job_class],
+                :method   => 'run',
+                :metadata => {
+                  :id        => attrs[:job_id],
+                  :queue     => attrs[:queue],
+                  :run_at    => attrs[:run_at].to_s,
+                  :priority  => attrs[:priority],
+                  :attempts  => attrs[:error_count].to_i
+                },
+                :params => attrs[:args]
+              ) { _run_without_appsignal }
+            end
+
+            alias_method :_run_without_appsignal, :_run
+            alias_method :_run, :_run_with_appsignal
+
+          end
         end
       end
     end
   end
 
   class Que::Job
-    prepend Appsignal::Integrations::QuePlugin
+    # For Ruby 2+ prepend would be more beautiful, but we support 1.9.3 too.
+    include Appsignal::Integrations::QuePlugin
   end
 end
