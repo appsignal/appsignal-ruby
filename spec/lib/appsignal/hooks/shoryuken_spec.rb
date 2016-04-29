a229925fffefb410f9e5249aabf3a5e32008551d
Stijn Mathysen <stijn@skylight.be>
2016-04-29 18:19:11 +0200

    cleanup integration; no need for an integration AND a hook file; merge all in one hook

--- spec/lib/appsignal/hooks/shoryuken_spec.rb
+++ spec/lib/appsignal/hooks/shoryuken_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Appsignal::Hooks::ShoryukenHook do
-  # context "with shoryuken" do
-  # end
+  context "with shoryuken" do
+    before(:all) do
+      module Shoryuken
+      end
+    end
+
+    after(:all) do
+      Object.send(:remove_const, :Shoryuken)
+    end
+
+    its(:dependencies_present?) { should be_true }
+  end
 
   context "without shoryuken" do
     its(:dependencies_present?) { should be_false }
