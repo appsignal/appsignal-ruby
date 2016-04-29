a138c672b55d105802a132b533182ebaad0093d8
Stijn Mathysen <stijn@skylight.be>
2016-04-29 20:25:08 +0200

    fix except method, which isnt available in all implementations on Hash

--- lib/appsignal/hooks/shoryuken.rb
+++ lib/appsignal/hooks/shoryuken.rb
@@ -5,7 +5,8 @@ module Appsignal
         metadata = {
           :queue => queue
         }
-        metadata.merge!(body.except('job_class', 'queue_name', 'arguments'))
+        exclude_keys = [ :job_class, :queue_name, :arguments ]
+        metadata.merge!(body.reject{ |key| exclude_keys.member?(key.to_sym) })
         metadata.merge!(sqs_msg.attributes)
 
         options = {
