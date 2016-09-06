bd891e8e41f51cc8434ce1eb630160443391f7ab
Robert Beekman <robert@matsimitsu.nl>
2016-09-06 11:14:36 +0200

    Ensure capistrano2 and 3 have configuration parity and add better error logging
    The documentation states that you can set a `stage` config and appsignal will treat the deploy as that environment, however capistrano2 did not have support for this feature.
    
    This commit adds support for the `stage` variable.
    
    It also improves error logging by adding environment name to the message when the environment is not active.

--- spec/lib/appsignal/capistrano3_spec.rb
+++ spec/lib/appsignal/capistrano3_spec.rb
@@ -196,7 +196,7 @@ if capistrano3_present?
           it "should not send deploy marker" do
             Appsignal::Marker.should_not_receive(:new)
             invoke('appsignal:deploy')
-            out_stream.string.should include("Not notifying of deploy, config is not active")
+            out_stream.string.should include("Not notifying of deploy, config is not active for environment: nonsense")
           end
         end
       end
