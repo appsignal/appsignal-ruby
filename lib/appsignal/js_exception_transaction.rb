appsignal/appsignal-ruby

commit f77788cbbc5b1558441294808122e751c5955bc8

Use GC::Profiler to track event and transaction GC
Instead of installing the Garbage Collection event hooks, setting
enable_gc_instrumentation in the AppSignal config now enables
GC::Profiler.

Appsignal::GarbageCollectionProfiler takes data from GC::Profiler
to report it to the extension whenever a transaction or event is started
or finished. It will also make sure to clear the internal garbage
collection profiler to stop it from leaking.

When the internal garbage collection profiler is slow to clear the total
GC time, Appsignal::GarbageCollectionProfiler#total_time uses a Mutex to
lock and make sure GC time is not counted twice.

The test for this sets a clear delay, which is used by FakeGCProfiler to
pause between returning the total time and clearing it. The test sets a
total time for the first thread, which gets cleared and should be 0 for
the second one.

In the tests, GC::Profiler.enable is stubbed in with gc instrumentation
enabled to make sure the test suite doesn't touch GC::Profiler.

FakeGCProfiler behaves like Ruby's internal GC::Profiler, but has a
writable @total_time for testing purposes. Using this instead of
stubbing the results of GC::Profiler.total_time allows for more a more
accurate replication of GC::Profiler's behavior by implementing a
working .clear method.

lib/appsignal/js_exception_transaction.rb
@@ -5,7 +5,7 @@ module Appsignal
     def initialize(data)
       @data = data
       @uuid = SecureRandom.uuid
-      @ext = Appsignal::Extension.start_transaction(@uuid, Appsignal::Transaction::FRONTEND)
+      @ext = Appsignal::Extension.start_transaction(@uuid, Appsignal::Transaction::FRONTEND, 0)
 
       set_action
       set_metadata

spec/lib/appsignal/js_exception_transaction_spec.rb
@@ -21,7 +21,7 @@ describe Appsignal::JSExceptionTransaction do
 
   describe "#initialize" do
     it "should call all required methods" do
-      expect( Appsignal::Extension ).to receive(:start_transaction).with('123abc', 'frontend').and_return(1)
+      expect( Appsignal::Extension ).to receive(:start_transaction).with('123abc', 'frontend', 0).and_return(1)
 
       expect( transaction ).to receive(:set_action)
       expect( transaction ).to receive(:set_metadata)

ext/agent.yml
@@ -1,18 +1,18 @@
 ---
-version: '2220960'
+version: de7a331
 triples:
   x86_64-linux:
-    checksum: 189daaef0fcc2fec442ac89227aefa2d3bef762d1cd7736706ae8ef79c40f5a3
-    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/2220960/appsignal-x86_64-linux-all-static.tar.gz
+    checksum: e2bbd606edf8c415b11129d0a9fca864606134e241d7e5c92e2acfe53d930ef6
+    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/de7a331/appsignal-x86_64-linux-all-static.tar.gz
   i686-linux:
-    checksum: 714e3759c085a97ec724bab092848513b9090c0ce3c7c8d182991916e592cbdb
-    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/2220960/appsignal-i686-linux-all-static.tar.gz
+    checksum: 65fbc606996f6ac40f961dc739f8fca85ec041e019b1299bb8176fd2a3fffaa3
+    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/de7a331/appsignal-i686-linux-all-static.tar.gz
   x86-linux:
-    checksum: 714e3759c085a97ec724bab092848513b9090c0ce3c7c8d182991916e592cbdb
-    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/2220960/appsignal-i686-linux-all-static.tar.gz
+    checksum: 65fbc606996f6ac40f961dc739f8fca85ec041e019b1299bb8176fd2a3fffaa3
+    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/de7a331/appsignal-i686-linux-all-static.tar.gz
   x86_64-darwin:
-    checksum: 9b591dee23cbeb31a05c49a1e5c143b7afb37348526d147957238722882c1f9e
-    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/2220960/appsignal-x86_64-darwin-all-static.tar.gz
+    checksum: cea10b64628f46cdfe881682ac10bd46f0d6ee7e320014b91eacaaa3c5ab956e
+    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/de7a331/appsignal-x86_64-darwin-all-static.tar.gz
   universal-darwin:
-    checksum: 9b591dee23cbeb31a05c49a1e5c143b7afb37348526d147957238722882c1f9e
-    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/2220960/appsignal-x86_64-darwin-all-static.tar.gz
+    checksum: cea10b64628f46cdfe881682ac10bd46f0d6ee7e320014b91eacaaa3c5ab956e
+    download_url: https://appsignal-agent-releases.global.ssl.fastly.net/de7a331/appsignal-x86_64-darwin-all-static.tar.gz

ext/appsignal_extension.c
@@ -44,15 +44,17 @@ static VALUE get_server_state(VALUE self, VALUE key) {
   }
 }
 
-static VALUE start_transaction(VALUE self, VALUE transaction_id, VALUE namespace) {
+static VALUE start_transaction(VALUE self, VALUE transaction_id, VALUE namespace, VALUE gc_duration_ms) {
   appsignal_transaction_t* transaction;
 
   Check_Type(transaction_id, T_STRING);
   Check_Type(namespace, T_STRING);
+  Check_Type(gc_duration_ms, T_FIXNUM);
 
   transaction = appsignal_start_transaction(
       make_appsignal_string(transaction_id),
-      make_appsignal_string(namespace)
+      make_appsignal_string(namespace),
+      NUM2LONG(gc_duration_ms)
   );
 
   if (transaction) {
@@ -62,23 +64,26 @@ static VALUE start_transaction(VALUE self, VALUE transaction_id, VALUE namespace
   }
 }
 
-static VALUE start_event(VALUE self) {
+static VALUE start_event(VALUE self, VALUE gc_duration_ms) {
   appsignal_transaction_t* transaction;
+
+  Check_Type(gc_duration_ms, T_FIXNUM);
 
   Data_Get_Struct(self, appsignal_transaction_t, transaction);
 
-  appsignal_start_event(transaction);
+  appsignal_start_event(transaction, NUM2LONG(gc_duration_ms));
 
   return Qnil;
 }
 
-static VALUE finish_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE body_format) {
+static VALUE finish_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE body_format, VALUE gc_duration_ms) {
   appsignal_transaction_t* transaction;
 
   Check_Type(name, T_STRING);
   Check_Type(title, T_STRING);
   Check_Type(body, T_STRING);
   Check_Type(body_format, T_FIXNUM);
+  Check_Type(gc_duration_ms, T_FIXNUM);
   Data_Get_Struct(self, appsignal_transaction_t, transaction);
 
   appsignal_finish_event(
@@ -86,12 +91,13 @@ static VALUE finish_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE
       make_appsignal_string(name),
       make_appsignal_string(title),
       make_appsignal_string(body),
-      FIX2INT(body_format)
+      FIX2INT(body_format),
+      FIX2LONG(gc_duration_ms)
   );
   return Qnil;
 }
 
-static VALUE record_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE duration, VALUE body_format) {
+static VALUE record_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE duration, VALUE body_format, VALUE gc_duration_ms) {
   appsignal_transaction_t* transaction;
   int duration_type;
 
@@ -103,6 +109,7 @@ static VALUE record_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE
       rb_raise(rb_eTypeError, "duration should be a Fixnum or Bignum");
   }
   Check_Type(body_format, T_FIXNUM);
+  Check_Type(gc_duration_ms, T_FIXNUM);
   Data_Get_Struct(self, appsignal_transaction_t, transaction);
 
   appsignal_record_event(
@@ -111,7 +118,8 @@ static VALUE record_event(VALUE self, VALUE name, VALUE title, VALUE body, VALUE
       make_appsignal_string(title),
       make_appsignal_string(body),
       NUM2LONG(duration),
-      FIX2INT(body_format)
+      FIX2INT(body_format),
+      NUM2LONG(gc_duration_ms)
   );
   return Qnil;
 }
@@ -194,13 +202,14 @@ static VALUE set_transaction_metadata(VALUE self, VALUE key, VALUE value) {
   return Qnil;
 }
 
-static VALUE finish_transaction(VALUE self) {
+static VALUE finish_transaction(VALUE self, VALUE gc_duration_ms) {
   appsignal_transaction_t* transaction;
   int sample;
 
+  Check_Type(gc_duration_ms, T_FIXNUM);
   Data_Get_Struct(self, appsignal_transaction_t, transaction);
 
-  sample = appsignal_finish_transaction(transaction);
+  sample = appsignal_finish_transaction(transaction, NUM2LONG(gc_duration_ms));
   return sample == 1 ? Qtrue : Qfalse;
 }
 
@@ -272,40 +281,12 @@ static void track_allocation(rb_event_flag_t flag, VALUE arg1, VALUE arg2, ID ar
   appsignal_track_allocation();
 }
 
-static void track_gc_start(rb_event_flag_t flag, VALUE arg1, VALUE arg2, ID arg3, VALUE arg4) {
-  appsignal_track_gc_start();
-}
-
-static void track_gc_end(rb_event_flag_t flag, VALUE arg1, VALUE arg2, ID arg3, VALUE arg4) {
-  appsignal_track_gc_end();
-}
-
 static VALUE install_allocation_event_hook() {
   // This event hook is only available on Ruby 2.1 and 2.2
   #if defined(RUBY_INTERNAL_EVENT_NEWOBJ)
   rb_add_event_hook(
       track_allocation,
       RUBY_INTERNAL_EVENT_NEWOBJ,
-      Qnil
-  );
-  #endif
-
-  return Qnil;
-}
-
-static VALUE install_gc_event_hooks() {
-  // These event hooks are only available on Ruby 2.1 and 2.2
-  #if defined(RUBY_INTERNAL_EVENT_GC_START)
-  rb_add_event_hook(
-      track_gc_start,
-      RUBY_INTERNAL_EVENT_GC_START,
-      Qnil
-  );
-  #endif
-  #if defined(RUBY_INTERNAL_EVENT_GC_END_MARK)
-  rb_add_event_hook(
-      track_gc_end,
-      RUBY_INTERNAL_EVENT_GC_END_MARK,
       Qnil
   );
   #endif
@@ -326,23 +307,22 @@ void Init_appsignal_extension(void) {
   rb_define_singleton_method(Extension, "get_server_state", get_server_state, 1);
 
   // Start transaction
-  rb_define_singleton_method(Extension, "start_transaction", start_transaction, 2);
+  rb_define_singleton_method(Extension, "start_transaction", start_transaction, 3);
 
   // Transaction instance methods
-  rb_define_method(ExtTransaction, "start_event",     start_event,                 0);
-  rb_define_method(ExtTransaction, "finish_event",    finish_event,                4);
-  rb_define_method(ExtTransaction, "record_event",    record_event,                5);
+  rb_define_method(ExtTransaction, "start_event",     start_event,                 1);
+  rb_define_method(ExtTransaction, "finish_event",    finish_event,                5);
+  rb_define_method(ExtTransaction, "record_event",    record_event,                6);
   rb_define_method(ExtTransaction, "set_error",       set_transaction_error,       3);
   rb_define_method(ExtTransaction, "set_sample_data", set_transaction_sample_data, 2);
   rb_define_method(ExtTransaction, "set_action",      set_transaction_action,      1);
   rb_define_method(ExtTransaction, "set_queue_start", set_transaction_queue_start, 1);
   rb_define_method(ExtTransaction, "set_metadata",    set_transaction_metadata,    2);
-  rb_define_method(ExtTransaction, "finish",          finish_transaction,          0);
+  rb_define_method(ExtTransaction, "finish",          finish_transaction,          1);
   rb_define_method(ExtTransaction, "complete",        complete_transaction,        0);
 
   // Event hook installation
   rb_define_singleton_method(Extension, "install_allocation_event_hook", install_allocation_event_hook, 0);
-  rb_define_singleton_method(Extension, "install_gc_event_hooks",        install_gc_event_hooks,        0);
 
   // Metrics
   rb_define_singleton_method(Extension, "set_gauge",              set_gauge,              2);

lib/appsignal.rb
@@ -58,11 +58,16 @@ module Appsignal
           Appsignal::Hooks.load_hooks
           Appsignal::EventFormatter.initialize_formatters
           initialize_extensions
-          Appsignal::Extension.install_allocation_event_hook if config[:enable_allocation_tracking]
+
+          if config[:enable_allocation_tracking]
+            Appsignal::Extension.install_allocation_event_hook
+          end
+
           if config[:enable_gc_instrumentation]
-            Appsignal::Extension.install_gc_event_hooks
+            GC::Profiler.enable
             Appsignal::Minutely.add_gc_probe
           end
+
           Appsignal::Minutely.start if config[:enable_minutely_probes]
           @subscriber = Appsignal::Subscriber.new
         else
@@ -325,6 +330,7 @@ require 'appsignal/hooks'
 require 'appsignal/marker'
 require 'appsignal/minutely'
 require 'appsignal/params_sanitizer'
+require 'appsignal/garbage_collection_profiler'
 require 'appsignal/integrations/railtie' if defined?(::Rails)
 require 'appsignal/integrations/resque'
 require 'appsignal/integrations/resque_active_job'

lib/appsignal/garbage_collection_profiler.rb
@@ -1,0 +1,47 @@
+module Appsignal
+  # Appsignal::GarbageCollectionProfiler wraps Ruby's GC::Profiler to be able
+  # to track garbage collection time for multiple transactions, while
+  # constantly clearing GC::Profiler's total_time to make sure it doesn't leak
+  # memory by keeping garbage collection run samples in memory.
+
+  class GarbageCollectionProfiler
+    def initialize
+      @total_time = 0
+    end
+
+    # Whenever #total_time is called, the current GC::Profiler.total_time gets
+    # added to @total_time, after which GC::Profiler.clear is called to prevent
+    # it from leaking memory. A class-level lock is used to make sure garbage
+    # collection time is never counted more than once.
+    #
+    # Whenever @total_time gets above two billion milliseconds (about 23 days),
+    # it's reset to make sure the result fits in a signed 32-bit integer.
+
+    def total_time
+      lock.synchronize do
+        @total_time += (internal_profiler.total_time * 1000).round
+        internal_profiler.clear
+      end
+
+      if @total_time > 2_000_000_000
+        @total_time = 0
+      end
+
+      @total_time
+    end
+
+    private
+
+    def self.lock
+      @lock ||= Mutex.new
+    end
+
+    def internal_profiler
+      GC::Profiler
+    end
+
+    def lock
+      self.class.lock
+    end
+  end
+end

lib/appsignal/transaction.rb
@@ -57,6 +57,10 @@ module Appsignal
       ensure
         Thread.current[:appsignal_transaction] = nil
       end
+
+      def garbage_collection_profiler
+        @garbage_collection_profiler ||= Appsignal::GarbageCollectionProfiler.new
+      end
     end
 
     attr_reader :ext, :transaction_id, :namespace, :request, :paused, :tags, :options, :discarded
@@ -72,7 +76,11 @@ module Appsignal
       @options = options
       @options[:params_method] ||= :params
 
-      @ext = Appsignal::Extension.start_transaction(@transaction_id, @namespace)
+      @ext = Appsignal::Extension.start_transaction(
+        @transaction_id,
+        @namespace,
+        self.class.garbage_collection_profiler.total_time
+      )
     end
 
     def nil_transaction?
@@ -84,7 +92,7 @@ module Appsignal
         Appsignal.logger.debug('Skipping transaction because it was manually discarded.'.freeze)
         return
       end
-      if @ext.finish
+      if @ext.finish(self.class.garbage_collection_profiler.total_time)
         sample_data
       end
       @ext.complete
@@ -195,7 +203,7 @@ module Appsignal
     alias_method :add_exception, :set_error
 
     def start_event
-      @ext.start_event
+      @ext.start_event(self.class.garbage_collection_profiler.total_time)
     end
 
     def finish_event(name, title, body, body_format=Appsignal::EventFormatter::DEFAULT)
@@ -203,7 +211,8 @@ module Appsignal
         name,
         title || BLANK,
         body || BLANK,
-        body_format || Appsignal::EventFormatter::DEFAULT
+        body_format || Appsignal::EventFormatter::DEFAULT,
+        self.class.garbage_collection_profiler.total_time
       )
     end
 

spec/lib/appsignal/extension_spec.rb
@@ -37,18 +37,18 @@ describe "extension loading and operation" do
       end
 
       context "with a transaction" do
-        subject { Appsignal::Extension.start_transaction('request_id', 'http_request') }
+        subject { Appsignal::Extension.start_transaction('request_id', 'http_request', 0) }
 
         it "should have a start_event method" do
-          subject.start_event
+          subject.start_event(0)
         end
 
         it "should have a finish_event method" do
-          subject.finish_event('name', 'title', 'body', 0)
+          subject.finish_event('name', 'title', 'body', 0, 0)
         end
 
         it "should have a record_event method" do
-          subject.record_event('name', 'title', 'body', 0, 1000)
+          subject.record_event('name', 'title', 'body', 1000, 0, 1000)
         end
 
         it "should have a set_error method" do
@@ -72,7 +72,7 @@ describe "extension loading and operation" do
         end
 
         it "should have a finish method" do
-          subject.finish
+          subject.finish(0)
         end
 
         it "should have a complete method" do

commit b6897e15359d26918b8d3c773bad93f760eba2c1

Fix JS transaction finish call
The finish call needs a GC end time, but a JS transaction has no
Ruby-time and no GC-time. Because of too much mocking the error wasn't
caught in the test suite.

Regression introduced in: f77788cbbc5b1558441294808122e751c5955bc8
adding a GC profiler to the Ruby gem.

lib/appsignal/js_exception_transaction.rb
@@ -47,7 +47,7 @@ module Appsignal
     end
 
     def complete!
-      @ext.finish
+      @ext.finish(0)
       @ext.complete
     end
   end

spec/lib/appsignal/js_exception_transaction_spec.rb
@@ -120,8 +120,8 @@ describe Appsignal::JSExceptionTransaction do
 
   describe "#complete!" do
     it "should call all required methods" do
-      expect(transaction.ext).to receive(:finish)
-      expect(transaction.ext).to receive(:complete)
+      expect(transaction.ext).to receive(:finish).and_call_original
+      expect(transaction.ext).to receive(:complete).and_call_original
       transaction.complete!
     end
   end