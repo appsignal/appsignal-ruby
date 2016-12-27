7bececb28e2f3b41505c77354a462ef54ff3b3e0
Tom de Bruijn <tom@tomdebruijn.com>
2016-12-27 13:30:24 +0100

    Fix previously pointless `private` definition
    Doesn't work for class methods unless in a singleton class definition.

--- lib/appsignal/system.rb
+++ lib/appsignal/system.rb
@@ -11,25 +11,27 @@ module Appsignal
     module Container
       CGROUP_FILE = "/proc/self/cgroup".freeze
 
-      def self.id
-        case cgroups
-        when %r{docker[-|/]([0-9a-f]+)}
-          $1
-        when %r{lxc/([0-9a-f-]+)$} # LXC / Heroku
-          $1
+      class << self
+        def id
+          case cgroups
+          when %r{docker[-|/]([0-9a-f]+)}
+            $1
+          when %r{lxc/([0-9a-f-]+)$} # LXC / Heroku
+            $1
+          end
         end
-      end
 
-      private
+        private
 
-      def self.cgroups
-        file = CGROUP_FILE
-        return unless File.exist? file
+        def cgroups
+          file = CGROUP_FILE
+          return unless File.exist? file
 
-        File.read(file)
-      rescue SystemCallError => e
-        Appsignal.logger.debug "Unable to read '#{file}' to determine cgroup"
-        Appsignal.logger.debug e
+          File.read(file)
+        rescue SystemCallError => e
+          Appsignal.logger.debug "Unable to read '#{file}' to determine cgroup"
+          Appsignal.logger.debug e
+        end
       end
     end
   end
