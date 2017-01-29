f5ca937153e431626412e934b4babf9b1e28e8c6
Tom de Bruijn <tom@tomdebruijn.com>
2017-01-29 14:58:31 +0100

    Update docs & specs for GC collection profiler

--- spec/lib/appsignal/garbage_collection_profiler_spec.rb
+++ spec/lib/appsignal/garbage_collection_profiler_spec.rb
@@ -1,44 +1,41 @@
 describe Appsignal::GarbageCollectionProfiler do
   let(:internal_profiler) { FakeGCProfiler.new }
+  let(:profiler) { described_class.new }
 
   before do
-    allow_any_instance_of(Appsignal::GarbageCollectionProfiler)
+    allow_any_instance_of(described_class)
       .to receive(:internal_profiler)
       .and_return(internal_profiler)
   end
 
-  it "should have a total time of 0" do
-    expect(Appsignal::GarbageCollectionProfiler.new.total_time).to eq(0)
+  context "on initialization" do
+    it "has a total time of 0" do
+      expect(profiler.total_time).to eq(0)
+    end
   end
 
-  describe "after a GC run" do
-    before do
-      internal_profiler.total_time = 0.12345
-      @profiler = Appsignal::GarbageCollectionProfiler.new
+  context "when the GC has run" do
+    before { internal_profiler.total_time = 0.12345 }
+
+    it "fetches the total time from Ruby's GC::Profiler" do
+      expect(profiler.total_time).to eq(123)
     end
 
-    it "should take the total time from Ruby's GC::Profiler" do
-      expect(@profiler.total_time).to eq(123)
-    end
-
-    it "should clear Ruby's GC::Profiler" do
+    it "clears Ruby's GC::Profiler afterward" do
       expect(internal_profiler).to receive(:clear)
-      @profiler.total_time
+      profiler.total_time
     end
   end
 
-  describe "when the total GC time becomes too high" do
-    it "should reset" do
-      profiler = Appsignal::GarbageCollectionProfiler.new
+  context "when the total GC time becomes too high" do
+    it "resets the total time" do
       internal_profiler.total_time = 2_147_483_647
       expect(profiler.total_time).to eq(0)
     end
   end
 
-  describe "after multiple GC runs" do
-    it "should add all times from Ruby's GC::Profiler together" do
-      profiler = Appsignal::GarbageCollectionProfiler.new
-
+  context "when the GC has run multiple times" do
+    it "adds all times from Ruby's GC::Profiler together" do
       2.times do
         internal_profiler.total_time = 0.12345
         profiler.total_time
@@ -48,8 +45,8 @@ describe Appsignal::GarbageCollectionProfiler do
     end
   end
 
-  describe "in multiple threads, with a slow GC::Profiler" do
-    it "should not count garbage collection times twice" do
+  context "when in multiple threads and with a slow GC::Profiler" do
+    it "does not count garbage collection times twice" do
       threads = []
       results = []
       internal_profiler.clear_delay = 0.001
