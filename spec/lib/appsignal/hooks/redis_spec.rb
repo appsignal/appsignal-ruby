8a7f46ae43c0e5b3cefbd0d33a735d4a117746f1
Tom de Bruijn <tom@tomdebruijn.com>
2017-02-19 18:53:13 +0100

    Don't force install Redis hook at in spec
    Calling the `.install` method directly always installs the hook even if
    the install condition isn't `true`.
    
    Let `Appsignal::Hooks.load_hooks` `try_to_install` the hook, which
    checks if the `dependencies_present?` check is `true`.
    
    Only run the "with redis" context when the Redis gem is present. This
    worked before because the specs that ran before "without redis" unset
    the "Redis" constant after the spec was ran even when it was originally
    loaded from the Redis gem.

--- spec/lib/appsignal/hooks/redis_spec.rb
+++ spec/lib/appsignal/hooks/redis_spec.rb
@@ -1,65 +1,55 @@
 describe Appsignal::Hooks::RedisHook do
   before do
     Appsignal.config = project_fixture_config
+    Appsignal::Hooks.load_hooks
   end
 
-  context "with redis" do
-    before :context do
-      class Redis
-        class Client
-          def process(_commands)
-            1
-          end
+  if DependencyHelper.redis_present?
+    context "with redis" do
+      context "with instrumentation enabled" do
+        before do
+          Appsignal.config.config_hash[:instrument_redis] = true
+          allow_any_instance_of(Redis::Client).to receive(:process_without_appsignal).and_return(1)
         end
-        VERSION = "1.0"
+
+        describe "#dependencies_present?" do
+          subject { described_class.new.dependencies_present? }
+
+          it { is_expected.to be_truthy }
+        end
+
+        it "should instrument a redis call" do
+          Appsignal::Transaction.create("uuid", Appsignal::Transaction::HTTP_REQUEST, "test")
+          expect(Appsignal::Transaction.current).to receive(:start_event)
+            .at_least(:once)
+          expect(Appsignal::Transaction.current).to receive(:finish_event)
+            .at_least(:once)
+            .with("query.redis", nil, nil, 0)
+
+          client = Redis::Client.new
+          expect(client.process([])).to eq 1
+        end
+      end
+
+      context "with instrumentation disabled" do
+        before do
+          Appsignal.config.config_hash[:instrument_redis] = false
+        end
+
+        describe "#dependencies_present?" do
+          subject { described_class.new.dependencies_present? }
+
+          it { is_expected.to be_falsy }
+        end
       end
     end
-    after(:context) { Object.send(:remove_const, :Redis) }
-
-    context "with instrumentation enabled" do
-      before do
-        Appsignal.config.config_hash[:instrument_redis] = true
-        Appsignal::Hooks::RedisHook.new.install
-      end
-
-      describe "#dependencies_present?" do
-        subject { described_class.new.dependencies_present? }
-
-        it { is_expected.to be_truthy }
-      end
-
-      it "should instrument a redis call" do
-        Appsignal::Transaction.create("uuid", Appsignal::Transaction::HTTP_REQUEST, "test")
-        expect(Appsignal::Transaction.current).to receive(:start_event)
-          .at_least(:once)
-        expect(Appsignal::Transaction.current).to receive(:finish_event)
-          .at_least(:once)
-          .with("query.redis", nil, nil, 0)
-
-        client = Redis::Client.new
-
-        expect(client.process([])).to eq 1
-      end
-    end
-
-    context "with instrumentation disabled" do
-      before do
-        Appsignal.config.config_hash[:instrument_redis] = false
-      end
-
+  else
+    context "without redis" do
       describe "#dependencies_present?" do
         subject { described_class.new.dependencies_present? }
 
         it { is_expected.to be_falsy }
       end
-    end
-  end
-
-  context "without redis" do
-    describe "#dependencies_present?" do
-      subject { described_class.new.dependencies_present? }
-
-      it { is_expected.to be_falsy }
     end
   end
 end
