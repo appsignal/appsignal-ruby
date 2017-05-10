5d6d8e36d61015fd889b380e8aa6a81f5b067969
Tom de Bruijn <tom@tomdebruijn.com>
2017-05-10 23:01:25 +0200

    Rework ActionCable integration
    Problem with this setup is that we can't set the params of the actual
    `perform_action` right now, see code comment.

--- lib/appsignal/hooks/action_cable.rb
+++ lib/appsignal/hooks/action_cable.rb
@@ -10,34 +10,81 @@ module Appsignal
       end
 
       def install
-        ActiveSupport::Notifications.subscribe("perform_action.action_cable", Subscriber.new)
-      end
+        ActionCable::Channel::Base.class_eval do
+          alias_method :original_perform_action, :perform_action
 
-      class Subscriber
-        def start(_name, id, payload)
-          # TODO: Set params and filter them, perferably with Rails param
-          # filtering if present. The params given are not filtered.
+          def perform_action(*args, &block)
+            # Request is only the original websocket request
+            request = ActionDispatch::Request.new(connection.env)
+            transaction = Appsignal::Transaction.create(
+              request.request_id,
+              Appsignal::Transaction::ACTION_CABLE,
+              request
+            )
 
-          # request = ActionDispatch::Request.new(payload)
-          Appsignal::Transaction.create(
-            id,
-            Appsignal::Transaction::ACTION_CABLE,
-            {}#,
-            # :params_method => :filtered_parameters
-          )
+            begin
+              original_perform_action(*args, &block)
+            rescue => exception
+              transaction.set_error(exception)
+              raise exception
+            ensure
+              # Cannot do this because it gets overwritten with the
+              # request.params from the original websocket request. We cannot
+              # set the params here right now, unless we make it possible to
+              # customize the params in the transaction.
+              # transaction.set_params(args.first)
+              # and use that when it is set, otherwise fall back on the
+              # request.params
+              # transaction.set_sample_data(:params, args.first)
+
+              transaction.set_action_if_nil("#{self.class}##{args.first["action"]}")
+              transaction.set_metadata("path", request.path)
+              transaction.set_metadata("method", "websocket")
+              Appsignal::Transaction.complete_current!
+            end
+          end
         end
 
-        def finish(_name, _id, payload)
-          transaction = Appsignal::Transaction.current
-
-          exception = payload[:exception_object]
-          transaction.set_error(exception) if exception
-
-          transaction.set_action_if_nil(
-            "#{payload[:channel_class]}##{payload[:action]}"
+        ActionCable::Channel::Base.set_callback :subscribe, :around, :prepend => true do |channel, inner|
+          # Request is only the original websocket request
+          request = ActionDispatch::Request.new(channel.connection.env)
+          transaction = Appsignal::Transaction.create(
+            request.request_id,
+            Appsignal::Transaction::ACTION_CABLE,
+            request
           )
-          transaction.set_metadata("method", "websocket")
-          transaction.complete
+
+          begin
+            inner.call
+          ensure
+            # Cannot do this, only ask for the params from the orignal websocket request
+            # transaction.set_sample_data(:params, channel.params)
+            transaction.set_action_if_nil("#{channel.class}#subscribed")
+            transaction.set_metadata("path", request.path)
+            transaction.set_metadata("method", "websocket")
+            Appsignal::Transaction.complete_current!
+          end
+        end
+
+        ActionCable::Channel::Base.set_callback :unsubscribe, :around, :prepend => true do |channel, inner|
+          # Request is only the original websocket request
+          request = ActionDispatch::Request.new(channel.connection.env)
+          transaction = Appsignal::Transaction.create(
+            request.request_id,
+            Appsignal::Transaction::ACTION_CABLE,
+            request
+          )
+
+          begin
+            inner.call
+          ensure
+            # Cannot do this, only ask for the params from the orignal websocket request
+            # transaction.set_sample_data(:params, channel.params)
+            transaction.set_action_if_nil("#{channel.class}#unsubscribed")
+            transaction.set_metadata("path", request.path)
+            transaction.set_metadata("method", "websocket")
+            Appsignal::Transaction.complete_current!
+          end
         end
       end
     end
