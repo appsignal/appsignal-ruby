3cb042cf2b93516837c61553672260302a7b9b5d
Tom de Bruijn <tom@tomdebruijn.com>
2017-11-02 16:03:49 +0100

    Support Sidekiq delay extension
    Import logic from the sidekiq/api library for display_class and
    display_args. This adds support for the Sidekiq delay extension and
    better supports most ActiveJob classes, along with ActionMailer classes.
    
    This is the second attempt at fixing this. The first attempt directly
    used classes from sidekiq/api, Sidekiq::Job to get the class name and
    arguments.
    
    Downside to this was the dependency on a internal Sidekiq class and
    loss of backwards compatibility for certain older versions.

--- lib/appsignal/hooks/sidekiq.rb
+++ lib/appsignal/hooks/sidekiq.rb
@@ -1,50 +1,7 @@
+require "yaml"
+
 module Appsignal
   class Hooks
-    # @api private
-    class SidekiqPlugin
-      include Appsignal::Hooks::Helpers
-
-      def job_keys
-        @job_keys ||= Set.new(%w(
-          class args retried_at failed_at
-          error_message error_class backtrace
-          error_backtrace enqueued_at retry
-          jid retry created_at wrapped
-        ))
-      end
-
-      def call(_worker, item, _queue)
-        args =
-          if item["class"] == "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper"
-            item["args"].first["arguments"]
-          else
-            item["args"]
-          end
-        params = Appsignal::Utils::ParamsSanitizer.sanitize args,
-          :filter_parameters => Appsignal.config[:filter_parameters]
-
-        Appsignal.monitor_transaction(
-          "perform_job.sidekiq",
-          :class       => item["wrapped"] || item["class"],
-          :method      => "perform",
-          :metadata    => formatted_metadata(item),
-          :params      => params,
-          :queue_start => item["enqueued_at"],
-          :queue_time  => (Time.now.to_f - item["enqueued_at"].to_f) * 1000
-        ) do
-          yield
-        end
-      end
-
-      def formatted_metadata(item)
-        {}.tap do |hsh|
-          item.each do |key, val|
-            hsh[key] = truncate(string_or_inspect(val)) unless job_keys.include?(key)
-          end
-        end
-      end
-    end
-
     class SidekiqHook < Appsignal::Hooks::Hook
       register :sidekiq
 
@@ -58,6 +15,128 @@ module Appsignal
             chain.add Appsignal::Hooks::SidekiqPlugin
           end
         end
+      end
+    end
+
+    # @api private
+    class SidekiqPlugin
+      include Appsignal::Hooks::Helpers
+
+      # TODO: Make constant
+      def job_keys
+        @job_keys ||= Set.new(%w(
+          class args retried_at failed_at
+          error_message error_class backtrace
+          error_backtrace enqueued_at retry
+          jid retry created_at wrapped
+        ))
+      end
+
+      def call(_worker, item, _queue)
+        transaction = Appsignal::Transaction.create(
+          SecureRandom.uuid,
+          Appsignal::Transaction::BACKGROUND_JOB,
+          Appsignal::Transaction::GenericRequest.new(
+            :queue_start => item["enqueued_at"]
+          )
+        )
+
+        Appsignal.instrument "perform_job.sidekiq" do
+          begin
+            yield
+          rescue Exception => exception # rubocop:disable Lint/RescueException
+            transaction.set_error(exception)
+            raise exception
+          end
+        end
+      ensure
+        if transaction
+          transaction.set_action_if_nil(formatted_action_name(item))
+          transaction.params = filtered_arguments(item)
+          formatted_metadata(item).each do |key, value|
+            transaction.set_metadata key, value
+          end
+          transaction.set_http_or_background_queue_start
+          Appsignal::Transaction.complete_current!
+        end
+      end
+
+      private
+
+      def formatted_action_name(job)
+        sidekiq_action_name = parse_action_name(job)
+        return sidekiq_action_name if sidekiq_action_name =~ /\.|#/
+        "#{sidekiq_action_name}#perform"
+      end
+
+      def filtered_arguments(job)
+        Appsignal::Utils::ParamsSanitizer.sanitize(
+          parse_arguments(job),
+          :filter_parameters => Appsignal.config[:filter_parameters]
+        )
+      end
+
+      def formatted_metadata(item)
+        {}.tap do |hash|
+          (item || {}).each do |key, value|
+            next if job_keys.include?(key)
+            hash[key] = truncate(string_or_inspect(value))
+          end
+        end
+      end
+
+      # Based on: https://github.com/mperham/sidekiq/blob/63ee43353bd3b753beb0233f64865e658abeb1c3/lib/sidekiq/api.rb#L316-L334
+      def parse_action_name(job)
+        args = job["args"]
+        case job["class"]
+        when /\ASidekiq::Extensions::Delayed/
+          safe_load(job["args"][0], job["class"]) do |target, method, _|
+            "#{target}.#{method}"
+          end
+        when "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper"
+          job_class = job["wrapped"] || args[0]
+          if "ActionMailer::DeliveryJob" == job_class
+            # MailerClass#mailer_method
+            args[0]["arguments"][0..1].join("#")
+          else
+            job_class
+          end
+        else
+          job["class"]
+        end
+      end
+
+      # Based on: https://github.com/mperham/sidekiq/blob/63ee43353bd3b753beb0233f64865e658abeb1c3/lib/sidekiq/api.rb#L336-L358
+      def parse_arguments(job)
+        args = job["args"]
+        case job["class"]
+        when /\ASidekiq::Extensions::Delayed/
+          safe_load(args[0], args) do |_, _, arg|
+            arg
+          end
+        when "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper"
+          is_wrapped = job["wrapped"]
+          job_args = is_wrapped ? args[0]["arguments"] : []
+          if "ActionMailer::DeliveryJob" == (is_wrapped || args[0])
+            # Remove MailerClass, mailer_method and "deliver_now"
+            job_args.drop(3)
+          else
+            job_args
+          end
+        else
+          args
+        end
+      end
+
+      # Based on: https://github.com/mperham/sidekiq/blob/63ee43353bd3b753beb0233f64865e658abeb1c3/lib/sidekiq/api.rb#L403-L412
+      def safe_load(content, default)
+        yield(*YAML.load(content))
+      rescue => error
+        # Sidekiq issue #1761: in dev mode, it's possible to have jobs enqueued
+        # which haven't been loaded into memory yet so the YAML can't be
+        # loaded.
+        Appsignal.logger.warn "Unable to load YAML: #{error.message}"
+        default
       end
     end
   end
