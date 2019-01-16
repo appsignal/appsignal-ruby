appsignal/appsignal-ruby

commit 876cce61be31b47367dc12ec18d090d1865efb5f

Fix jRuby publish by making a custom gemspec (#377)
A problem with publishing the jRuby version of the gem is that you will
need to have jRuby installed locally in order for it to pick up the
gemspec file. This is because it's only specified as `gem.platform =
"java"` if the Ruby version is jRuby.

When you're running a non-jRuby Ruby version it will not find the gem
version for jRuby when publishing the gem. This causes the `rake
publish` task to error and exit early, only shipping the normal Ruby gem
version.

Instead, configure the gemspec in the Rake tasks dynamically and
configure a `Gem::PackageTask` for that gemspec. Then, when calling
those special tasks it will build the correct version, allowing us to
ship it without needing to switch to jRuby in the Rake task itself
(which would require knowledge of which Ruby version manager is
installed locally).

Rakefile
@@ -1,4 +1,5 @@
 require "bundler"
+require "rubygems/package_task"
 require "fileutils"
 
 GEMFILES = %w[
@@ -38,75 +39,116 @@ VERSION_MANAGERS = {
   :rvm => ->(version) { "rvm use --default #{version.split("-").first}" }
 }.freeze
 
-task :publish do
-  require "appsignal/version"
+namespace :build do
+  def modify_base_gemspec
+    eval(File.read("appsignal.gemspec")).tap do |s| # rubocop:disable Security/Eval
+      yield s
+    end
+  end
 
-  NAME = "appsignal".freeze
+  namespace :ruby do
+    spec = modify_base_gemspec do |s|
+      s.extensions = %w[ext/extconf.rb]
+    end
+
+    Gem::PackageTask.new(spec) { |_pkg| }
+  end
+
+  namespace :jruby do
+    spec = modify_base_gemspec do |s|
+      s.platform = "java"
+      s.extensions = %w[ext/Rakefile]
+      s.add_dependency "ffi"
+    end
+
+    Gem::PackageTask.new(spec) { |_pkg| }
+  end
+
+  desc "Build all gem versions"
+  task :all => ["ruby:gem", "jruby:gem"]
+
+  desc "Clean up all gem build artifacts"
+  task :clean do
+    FileUtils.rm_rf File.expand_path("../pkg", __FILE__)
+  end
+end
+
+namespace :publish do
   VERSION_FILE = "lib/appsignal/version.rb".freeze
   CHANGELOG_FILE = "CHANGELOG.md".freeze
 
-  raise "$EDITOR should be set" unless ENV["EDITOR"]
-
-  def build_and_push_gem
-    puts "# Building gem"
-    FileUtils.rm_f("#{NAME}-#{gem_version}.gem")
-    puts `gem build #{NAME}.gemspec`
-    puts "# Publishing Gem"
-    puts `gem push #{NAME}-#{gem_version}.gem`
-  end
-
-  def create_and_push_tag
-    puts `git commit -am 'Bump to #{version} [ci skip]'`
-    puts "# Creating tag #{version}"
-    puts `git tag #{version}`
-    puts `git push origin #{version}`
-    puts `git push origin #{current_branch}`
-  rescue
-    raise "Tag: '#{version}' already exists"
-  end
-
   def changes
     git_status_to_array(`git status -s -u`)
-  end
-
-  def gem_version
-    Appsignal::VERSION
-  end
-
-  def version
-    @version ||= "v#{gem_version}"
-  end
-
-  def current_branch
-    `git rev-parse --abbrev-ref HEAD`.chomp
   end
 
   def git_status_to_array(changes)
     changes.split("\n").each { |change| change.gsub!(/^.. /, "") }
   end
 
-  raise "Branch should hold no uncommitted file change)" unless changes.empty?
-
-  system("$EDITOR #{VERSION_FILE}")
-  unless changes.member?(VERSION_FILE)
-    raise "Actually change the version in: #{VERSION_FILE}"
+  def current_branch
+    `git rev-parse --abbrev-ref HEAD`.chomp
   end
 
-  Appsignal.send(:remove_const, :VERSION)
-  load File.expand_path(VERSION_FILE)
-  system("$EDITOR #{CHANGELOG_FILE}")
+  task :check_requirements do
+    unless changes.empty?
+      puts "ERROR: There should be no uncommitted file changes."
+      exit 1
+    end
+    unless ENV["EDITOR"]
+      puts "ERROR: $EDITOR environment variable should be set."
+      exit 1
+    end
+  end
 
-  # Build and push for MRI
-  ENV.delete("APPSIGNAL_PUSH_JAVA_GEM")
-  build_and_push_gem
+  task :configure_version do
+    puts "\n# Configuring new gem version"
 
-  # Build and push for jRuby
-  ENV["APPSIGNAL_PUSH_JAVA_GEM"] = "true"
-  build_and_push_gem
+    puts `$EDITOR #{VERSION_FILE}`
+    unless changes.member?(VERSION_FILE)
+      puts "ERROR: Please actually change the gem version in: #{VERSION_FILE}"
+      exit 1
+    end
 
-  # Create tag
-  create_and_push_tag
+    puts "\n# Updating the changelog"
+    puts `$EDITOR #{CHANGELOG_FILE}`
+  end
+
+  task :push_gem_packages do
+    puts "\n# Pushing gem packages"
+    Dir.chdir("#{File.dirname(__FILE__)}/pkg") do
+      Dir["*.gem"].each do |gem_package|
+        puts "## Publishing gem package: #{gem_package}"
+        puts `gem push #{gem_package}`
+      end
+    end
+  end
+
+  task :tag_and_push_version do
+    # Make sure to load the new version number
+    Appsignal.send(:remove_const, :VERSION)
+    load File.expand_path(VERSION_FILE)
+    version = "v#{Appsignal::VERSION}"
+
+    begin
+      puts `git commit -am 'Bump to #{version} [ci skip]'`
+      puts "# Creating tag #{version}"
+      puts `git tag #{version}`
+      puts `git push origin #{version}`
+      puts `git push origin #{current_branch}`
+    rescue
+      puts "ERROR: Tag '#{version}' already exists"
+      exit 1
+    end
+  end
 end
+task :publish => [
+  "publish:check_requirements",
+  "publish:configure_version",
+  "build:clean",
+  "build:all",
+  "publish:push_gem_packages",
+  "publish:tag_and_push_version"
+]
 
 desc "Install the AppSignal gem, extension and all possible dependencies."
 task :install => "extension:install" do

appsignal.gemspec
@@ -2,7 +2,7 @@
 
 require File.expand_path("../lib/appsignal/version", __FILE__)
 
-Gem::Specification.new do |gem| # rubocop:disable Metrics/BlockLength
+Gem::Specification.new do |gem|
   gem.authors = [
     "Robert Beekman",
     "Thijs Cadier"
@@ -21,14 +21,6 @@ Gem::Specification.new do |gem| # rubocop:disable Metrics/BlockLength
   gem.require_paths         = %w[lib ext]
   gem.version               = Appsignal::VERSION
   gem.required_ruby_version = ">= 1.9"
-
-  if RUBY_PLATFORM == "java" || ENV["APPSIGNAL_PUSH_JAVA_GEM"]
-    gem.platform = "java"
-    gem.extensions = %w[ext/Rakefile]
-    gem.add_dependency "ffi"
-  else
-    gem.extensions = %w[ext/extconf.rb]
-  end
 
   gem.add_dependency "rack"
 

.gitignore
@@ -31,3 +31,4 @@ ext/Makefile
 ext/*.bundle
 ext/*.o
 ext/*.so
+pkg/

commit ae09a5f580fc34c8a6c2cc14fe2356b2dd7c784f

Add extensions definition to gemspec
This fixes the issue that the AppSignal gem doesn't install through
Bundler when specifying a git source:

```
gem "appsignal",
  :git => "https://github.com/appsignal/appsignal-ruby.git",
  :branch => "master"
```

This change adds the `extensions` config option as described here:
https://guides.rubygems.org/specification-reference/#extensions
by default for the Ruby C-extension. This means it does not work for
JRuby when installing through a git source.

This was broken in #377 where we generate the gemspecs for Ruby and
JRuby during the "rake publish" process.

We only ask users to use the git source when testing something small. So
full support for all implementations (JRuby) doesn't seem necessary at
this time. We don't recommend users to rely on the git source
installation method all the time.

Rakefile
@@ -40,23 +40,26 @@ VERSION_MANAGERS = {
 }.freeze
 
 namespace :build do
+  def base_gemspec
+    eval(File.read("appsignal.gemspec")) # rubocop:disable Security/Eval
+  end
+
   def modify_base_gemspec
-    eval(File.read("appsignal.gemspec")).tap do |s| # rubocop:disable Security/Eval
+    base_gemspec.tap do |s|
       yield s
     end
   end
 
   namespace :ruby do
-    spec = modify_base_gemspec do |s|
-      s.extensions = %w[ext/extconf.rb]
-    end
-
-    Gem::PackageTask.new(spec) { |_pkg| }
+    # Extension default set in `appsignal.gemspec`
+    Gem::PackageTask.new(base_gemspec) { |_pkg| }
   end
 
   namespace :jruby do
     spec = modify_base_gemspec do |s|
       s.platform = "java"
+      # Override extensions config with JRuby extension installer
+      # Default set in `appsignal.gemspec`
       s.extensions = %w[ext/Rakefile]
       s.add_dependency "ffi"
     end

appsignal.gemspec
@@ -21,6 +21,9 @@ Gem::Specification.new do |gem|
   gem.require_paths         = %w[lib ext]
   gem.version               = Appsignal::VERSION
   gem.required_ruby_version = ">= 1.9"
+  # Default extension installer. Overridden by JRuby gemspec as defined in
+  # `Rakefile`.
+  gem.extensions            = %w[ext/extconf.rb]
 
   gem.add_dependency "rack"
 