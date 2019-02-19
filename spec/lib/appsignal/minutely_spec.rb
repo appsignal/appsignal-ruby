6c06b149fe9277fa7db43524f5469c4cbbdd0909
Tom de Bruijn <tom@tomdebruijn.com>
2019-02-19 15:05:32 +0100

    Add wait_for helper that checks conditions in spec
    Only using `sleep X` is a little brittle as some machines are slower
    than others. On faster machines you don't want to wait for the longest
    time it can possibly take. So instead perform more checks in the helper
    and set a maximum time after which it should have been completed.
    
    The `wait_for` helper will check a condition for 5 seconds before giving
    up and raising an error, causing the spec to fail.

--- spec/lib/appsignal/minutely_spec.rb
+++ spec/lib/appsignal/minutely_spec.rb
@@ -42,9 +42,8 @@ describe Appsignal::Minutely do
       probe = Probe.new
       Appsignal::Minutely.probes << probe
       Appsignal::Minutely.start
-      sleep 0.01
 
-      expect(probe.calls).to be >= 2
+      wait_for("enough probe calls") { probe.calls >= 2 }
       expect(log).to include("Gathering minutely metrics with 1 probe")
       expect(log).to include("Gathering minutely metrics with Probe probe")
     end
@@ -56,16 +55,42 @@ describe Appsignal::Minutely do
         Appsignal::Minutely.probes << probe
         Appsignal::Minutely.probes << broken_probe
         Appsignal::Minutely.start
-        sleep 0.01
 
-        expect(probe.calls).to be >= 2
-        expect(broken_probe.calls).to be >= 2
+        wait_for("enough probe calls") { probe.calls >= 2 }
+        wait_for("enough broken_probe calls") { broken_probe.calls >= 2 }
 
         expect(log).to include("Gathering minutely metrics with 2 probes")
         expect(log).to include("Gathering minutely metrics with Probe probe")
         expect(log).to include("Gathering minutely metrics with BrokenProbe probe")
         expect(log).to include("Error in minutely thread (BrokenProbe): oh no!")
       end
+    end
+
+    # Wait for a condition to be met
+    #
+    # @example
+    #   # Perform threaded operation
+    #   wait_for("enough probe calls") { probe.calls >= 2 }
+    #   # Assert on result
+    #
+    # @param name [String] The name of the condition to check. Used in the
+    #   error when it fails.
+    # @yield Assertion to check.
+    # @yieldreturn [Boolean] True/False value that indicates if the condition
+    #   is met.
+    # @raise [StandardError] Raises error if the condition is not met after 5
+    #   seconds, 5_000 tries.
+    def wait_for(name)
+      max_wait = 5_000
+      i = 0
+      while i <= max_wait
+        break if yield
+        i += 1
+        sleep 0.001
+      end
+
+      return unless i == max_wait
+      raise "Waited 5 seconds for #{name} condition, but was not met."
     end
   end
 
