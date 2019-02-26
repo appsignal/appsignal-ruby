141a0633bd6f340e4fdfb4f344a36c7656606a8f
Tom de Bruijn <tom@tomdebruijn.com>
2019-02-26 14:03:32 +0100

    Fix YAML deprecation warning (#479)
    On Ruby 2.6 the Psych/YAML Ruby class prefers keyword arguments. Update
    the code to use keyword arguments on newer Rubies.
    
    ```
    <path>/appsignal/gem/lib/appsignal/cli/diagnose.rb:337:
      Passing permitted_classes with the 2nd argument of Psych.safe_load is
      deprecated. Use keyword argument like Psych.safe_load(yaml,
      permitted_classes: ...) instead.
    ```
    
    Also remove the `permitted_classes` argument value from being passed to
    `YAML.load`. The second argument on `YAML.load` is `filename`, not
    `permitted_classes`, so omit it entirely for Ruby 2.0 and lower.

--- lib/appsignal/cli/diagnose/utils.rb
+++ lib/appsignal/cli/diagnose/utils.rb
@@ -30,6 +30,23 @@ module Appsignal
 
           IO.binread(path, length, offset)
         end
+
+        def self.parse_yaml(contents)
+          arguments = [contents]
+          if YAML.respond_to? :safe_load
+            method = :safe_load
+            arguments << \
+              if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.6.0")
+                # Use keyword params for Ruby 2.6 and up
+                { :permitted_classes => [Time] }
+              else
+                [Time]
+              end
+          else
+            method = :load
+          end
+          YAML.send(method, *arguments)
+        end
       end
     end
   end
