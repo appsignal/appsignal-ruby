0713e34a8710e0ad27ecab8342802d9e13241dad
Tom de Bruijn <tom@tomdebruijn.com>
2019-06-08 15:45:12 +0200

    Explicitly configure which RSpec syntax is used
    We use the `expect` syntax in our test suites.
    
    This additional configuration disables the `should` syntax from being
    available.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -72,6 +72,12 @@ RSpec.configure do |config|
 
   config.example_status_persistence_file_path = "spec/examples.txt"
   config.fail_if_no_examples = true
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+  end
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
 
   def spec_system_tmp_dir
     File.join(tmp_dir, "system-tmp")
