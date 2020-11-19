ca9cfb00a15a6bc5e4238d7eb39fdb9f0ee4ffc6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-19 13:42:26 +0100

    Switch to using Module.prepend instead of method aliasing in unicorn hook

---
+++ lib/appsignal/integrations/unicorn.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Since version 1.0 requiring this file is not necessary anymore to get
+# Rake integration.
+#
+module Appsignal
+  module Integrations
+    module UnicornIntegration
+      # Make sure that appsignal is started and the last transaction
+      # in a worker gets flushed.
+      #
+      # We'd love to be able to hook this into Unicorn in a less
+      # intrusive way, but this is the best we can do given the
+      # options we have.
+      
+      module Server
+        def worker_loop(worker)
+          Appsignal.forked
+          super
+        end
+      end
+
+      module Worker
+        def close
+          Appsignal.stop("unicorn")
+          super
+        end
+      end
+    end
+  end
+end
