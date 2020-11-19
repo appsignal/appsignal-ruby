1b8393a7d87b7dd2c9d45b091b5cf2e3f07645a9
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-19 14:50:58 +0100

    fixup! Switch to using Module.prepend instead of method aliasing in webmachine hook

--- lib/appsignal/integrations/webmachine.rb
+++ lib/appsignal/integrations/webmachine.rb
@@ -4,35 +4,35 @@ module Appsignal
   module Integrations
     # @api private
     module WebmachineIntegration
-        def run
-          transaction = Appsignal::Transaction.create(
-            SecureRandom.uuid,
-            Appsignal::Transaction::HTTP_REQUEST,
-            request,
-            :params_method => :query
-          )
+      def run
+        transaction = Appsignal::Transaction.create(
+          SecureRandom.uuid,
+          Appsignal::Transaction::HTTP_REQUEST,
+          request,
+          :params_method => :query
+        )
 
-          transaction.set_action_if_nil("#{resource.class.name}##{request.method}")
+        transaction.set_action_if_nil("#{resource.class.name}##{request.method}")
 
-          Appsignal.instrument("process_action.webmachine") do
-            super
-          end
-
-          Appsignal::Transaction.complete_current!
+        Appsignal.instrument("process_action.webmachine") do
+          super
         end
 
-        private
+        Appsignal::Transaction.complete_current!
+      end
 
-        def handle_exceptions
-          super do
-            begin
-              yield
-            rescue Exception => e # rubocop:disable Lint/RescueException
-              Appsignal.set_error(e)
-              raise e
-            end
+      private
+
+      def handle_exceptions
+        super do
+          begin
+            yield
+          rescue Exception => e # rubocop:disable Lint/RescueException
+            Appsignal.set_error(e)
+            raise e
           end
         end
+      end
     end
   end
 end
