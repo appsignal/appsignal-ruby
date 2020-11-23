44fb788aa78c28a481a407c13fe4622d541f40bb
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-23 14:37:04 +0100

    Switch to using Module.prepend instead of method aliasing in que hook

--- lib/appsignal/integrations/que.rb
+++ lib/appsignal/integrations/que.rb
@@ -3,42 +3,35 @@
 module Appsignal
   module Integrations
     module QuePlugin
-      def self.included(base)
-        base.class_eval do
-          def _run_with_appsignal(*)
-            local_attrs = respond_to?(:que_attrs) ? que_attrs : attrs
-            env = {
-              :metadata    => {
-                :id        => local_attrs[:job_id] || local_attrs[:id],
-                :queue     => local_attrs[:queue],
-                :run_at    => local_attrs[:run_at].to_s,
-                :priority  => local_attrs[:priority],
-                :attempts  => local_attrs[:error_count].to_i
-              },
-              :params => local_attrs[:args]
-            }
+      def _run(*)
+        local_attrs = respond_to?(:que_attrs) ? que_attrs : attrs
+        env = {
+          :metadata    => {
+            :id        => local_attrs[:job_id] || local_attrs[:id],
+            :queue     => local_attrs[:queue],
+            :run_at    => local_attrs[:run_at].to_s,
+            :priority  => local_attrs[:priority],
+            :attempts  => local_attrs[:error_count].to_i
+          },
+          :params => local_attrs[:args]
+        }
 
-            request = Appsignal::Transaction::GenericRequest.new(env)
+        request = Appsignal::Transaction::GenericRequest.new(env)
 
-            transaction = Appsignal::Transaction.create(
-              SecureRandom.uuid,
-              Appsignal::Transaction::BACKGROUND_JOB,
-              request
-            )
+        transaction = Appsignal::Transaction.create(
+          SecureRandom.uuid,
+          Appsignal::Transaction::BACKGROUND_JOB,
+          request
+        )
 
-            begin
-              Appsignal.instrument("perform_job.que") { _run_without_appsignal }
-            rescue Exception => error # rubocop:disable Lint/RescueException
-              transaction.set_error(error)
-              raise error
-            ensure
-              transaction.set_action_if_nil "#{local_attrs[:job_class]}#run"
-              Appsignal::Transaction.complete_current!
-            end
-          end
-
-          alias_method :_run_without_appsignal, :_run
-          alias_method :_run, :_run_with_appsignal
+        begin
+          Appsignal.instrument("perform_job.que") { super }
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          transaction.set_error(error)
+          raise error
+        ensure
+          transaction.set_action_if_nil "#{local_attrs[:job_class]}#run"
+          Appsignal::Transaction.complete_current!
         end
       end
     end
