326799fe6ca8fdd809dfd743732f6f09e3306a3f
Robert Beekman <robertbeekman@matsimitsu.nl>
2020-11-30 13:21:30 +0100

    Add hook to capture and emit ActionMailer metrics
    Used in the `ActionMailer` magic dashboard.
    
    Subscribes to ActionMailer's `process.action_mailer` notifications
    and emits `count` metrics for each call.

---
+++ lib/appsignal/hooks/action_mailer.rb
@@ -0,0 +1,22 @@
+module Appsignal
+  class Hooks
+    class ActionMailerHook < Appsignal::Hooks::Hook
+      register :action_mailer
+
+      def dependencies_present?
+        defined?(::ActionMailer)
+      end
+
+      def install
+        ActiveSupport::Notifications
+          .subscribe("process.action_mailer") do |_, _, _, _, payload|
+          Appsignal.increment_counter(
+            :action_mailer_process,
+            1,
+            :mailer => payload[:mailer], :action => payload[:action]
+          )
+        end
+      end
+    end
+  end
+end
