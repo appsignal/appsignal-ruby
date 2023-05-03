4c0d438e593031ac1d1489d9d64c452087834c68
Tom de Bruijn <tom@tomdebruijn.com>
2023-05-03 09:45:22 +0200

    Add more debug info to contain_log matcher (#965)
    The minutely_spec.rb spec sometimes fails with a mismatch in log lines,
    but I can't tell what is wrong. Print more complete output so it's
    hopefully easier to debug when it happens again.
    
    [skip changeset]
    [skip review]

--- spec/support/matchers/contains_log.rb
+++ spec/support/matchers/contains_log.rb
@@ -1,6 +1,18 @@
 RSpec::Matchers.define :contains_log do |level, message|
+  expected_log_line = "[#{level.upcase}] #{message}"
+
   match do |actual|
-    actual.include?("[#{level.upcase}] #{message}")
+    actual.include?(expected_log_line)
+  end
+
+  failure_message do |actual|
+    <<~MESSAGE
+      Did not contain log line:
+      #{expected_log_line}
+
+      Received logs:
+      #{actual}
+    MESSAGE
   end
 
   diffable
