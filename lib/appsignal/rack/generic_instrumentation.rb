e4214b8a173dc294706033e0e3356525e16fd29e
Julik Tarkhanov <me@julik.nl>
2024-02-04 14:10:07 +0100

    Monitor serving of Rack response bodies
    Some work might be getting done within a Rack response body. For example, when
    ActionController::Streaming is used, or when a Rack app elects to stream a response.
    
    The Rack SPEC doc actually defines the behavior in sufficient detail to wrap this
    into the same Appsignal transaction.
    
    The Rails instrumentation may also inherit from the more generic one as most
    methods and constants are shared then

--- lib/appsignal/rack/generic_instrumentation.rb
+++ lib/appsignal/rack/generic_instrumentation.rb
@@ -6,8 +6,79 @@ module Appsignal
   # @api private
   module Rack
     class GenericInstrumentation
+      class BodyWrapper
+        def initialize(appsignal_transaction, body)
+          @transaction = appsignal_transaction
+          @body = body
+        end
+
+        # This must be present in all Rack bodies and will be called by the serving adapter
+        def close
+          @body.close if @body.respond_to?(:close)
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          @transaction.set_error(error) if @transaction
+          raise error
+        ensure
+          @transaction.complete if @transaction
+        end
+      end
+
+      # The standard Rack body wrapper which exposes "each" for iterating
+      # over the response body. This is supported across all 3 major Rack
+      # versions.
+      class EnumerableBodyWrapper < BodyWrapper
+        def each(&blk)
+          return enum_for(:each) unless block_given?
+
+          @body.each do |bytes|
+            yield bytes
+          end
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          @transaction.set_error(error) if @transaction
+          raise error
+        end
+      end
+
+      # The callable wrapper is a new Rack 3.x feature, and would not work
+      # with older Rack versions. Also, it must not respond to `each` because
+      # "If it responds to each, you must call each and not call".
+      class CallableBodyWrapper < BodyWrapper
+        def call(_stream)
+          @body.call(_stream)
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          @transaction.set_error(error) if @transaction
+          raise error
+        end
+      end
+
+      # "to_ary" takes precedence over "each" and allows the response body
+      # to be read eagerly. If the body supports that method, it takes precedence
+      # over "each":
+      # "Middleware may call to_ary directly on the Body and return a new Body in its place"
+      class ArrayableBodyWrapper < EnumerableBodyWrapper
+        def to_ary
+          @body.to_ary
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          @transaction.set_error(error) if @transaction
+          raise error
+        ensure
+          close
+        end
+      end
+
+      # Having "to_path" on a body allows Rack to serve out a static file, or to
+      # pass that file to the downstream webserver for sending using X-Sendfile
+      class PathableBodyWrapper < EnumerableBodyWrapper
+        def to_path
+          @body.to_path
+        rescue Exception => error # rubocop:disable Lint/RescueException
+          @transaction.set_error(error) if @transaction
+          raise error
+        end
+      end
+
       def initialize(app, options = {})
-        Appsignal.internal_logger.debug "Initializing Appsignal::Rack::GenericInstrumentation"
+        Appsignal.internal_logger.debug "Initializing #{self.class.to_s}"
         @app = app
         @options = options
       end
@@ -16,7 +87,12 @@ module Appsignal
         if Appsignal.active?
           call_with_appsignal_monitoring(env)
         else
-          @app.call(env)
+          # Create the same body wrapping as when we are monitoring a transaction,
+          # so that the behavior of the Rack stack does not change just because
+          # Appsignal is off. Rack treats bodies in a special way which also
+          # differs between Rack versions, so it is important to keep it consistent
+          status, headers, obody = @app.call(env)
+          [status, headers, wrap_body(_transaction = nil, obody)]
         end
       end
 
@@ -28,8 +104,10 @@ module Appsignal
           request
         )
         begin
+          transaction.set_http_or_background_queue_start
           Appsignal.instrument("process_action.generic") do
-            @app.call(env)
+            status, headers, body = @app.call(env)
+            [status, headers, wrap_body(transaction, body)]
           end
         rescue Exception => error # rubocop:disable Lint/RescueException
           transaction.set_error(error)
@@ -38,8 +116,23 @@ module Appsignal
           transaction.set_action_if_nil(env["appsignal.route"] || "unknown")
           transaction.set_metadata("path", request.path)
           transaction.set_metadata("method", request.request_method)
-          transaction.set_http_or_background_queue_start
-          Appsignal::Transaction.complete_current!
+        end
+      end
+
+      def wrap_body(transaction, obody)
+        # The logic of how Rack treats a response body differs based on which methods
+        # the body responds to. This means that to support the Rack 3.x spec in full
+        # we need to return a wrapper which matches the API of the wrapped body as closely
+        # as possible. Pick the wrapper from the most specific to the least specific.
+        # See https://github.com/rack/rack/blob/main/SPEC.rdoc#the-body-
+        if obody.respond_to?(:to_path)
+          PathableBodyWrapper.new(transaction, obody)
+        elsif obody.respond_to?(:to_ary)
+          ArrayableBodyWrapper.new(transaction, obody)
+        elsif !obody.respond_to?(:each) && obody.respond_to?(:call)
+          CallableBodyWrapper.new(transaction, obody)
+        else
+          EnumerableBodyWrapper.new(transaction, obody)
         end
       end
     end
