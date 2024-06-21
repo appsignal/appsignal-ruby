487330d6c1398f240d9d6cb16207c5d2fc547b5a
Tom de Bruijn <tom@tomdebruijn.com>
2024-06-21 15:40:39 +0200

    Use AbstractMiddleware for Rails middleware
    Use our new AbstractMiddleware as a basis for the Rails instrumentation
    middleware. This moves all responsibility for handling nested
    instrumentation middleware to the AbstractMiddleware.
    
    The Rails middleware can now focus on only the part it needs to do: the
    action name and some additional metadata.
    
    The RailsInstrumentation middleware previously didn't create an event,
    but the AbstractMiddleware does do that now. It will report a new
    `middleware.rails` event now.
    
    I didn't add a changeset for this change as it's mostly an internal
    refactor.

--- lib/appsignal/rack/rails_instrumentation.rb
+++ lib/appsignal/rack/rails_instrumentation.rb
@@ -5,66 +5,24 @@ require "rack"
 module Appsignal
   module Rack
     # @api private
-    class RailsInstrumentation
+    class RailsInstrumentation < Appsignal::Rack::AbstractMiddleware
       def initialize(app, options = {})
-        Appsignal.internal_logger.debug "Initializing Appsignal::Rack::RailsInstrumentation"
-        @app = app
-        @options = options
+        options[:request_class] ||= ActionDispatch::Request
+        options[:params_method] ||= :filtered_parameters
+        options[:instrument_span_name] ||= "middleware.rails"
+        super
       end
 
-      def call(env)
-        if Appsignal.active?
-          call_with_appsignal_monitoring(env)
-        else
-          @app.call(env)
-        end
-      end
+      private
 
-      def call_with_appsignal_monitoring(env)
-        request = ActionDispatch::Request.new(env)
-        transaction = env.fetch(
-          Appsignal::Rack::APPSIGNAL_TRANSACTION,
-          Appsignal::Transaction::NilTransaction.new
-        )
+      def add_transaction_metadata_after(transaction, request)
+        controller = request.env["action_controller.instance"]
+        transaction.set_action_if_nil("#{controller.class}##{controller.action_name}") if controller
 
-        begin
-          @app.call(env)
-        rescue Exception => error # rubocop:disable Lint/RescueException
-          transaction.set_error(error)
-          raise error
-        ensure
-          controller = env["action_controller.instance"]
-          if controller
-            transaction.set_action_if_nil("#{controller.class}##{controller.action_name}")
-          end
-          transaction.set_params_if_nil(fetch_params(request))
-          request_id = fetch_request_id(env)
-          transaction.set_tags(:request_id => request_id) if request_id
-          transaction.set_metadata("path", request.path)
-          request_method = fetch_request_method(request)
-          transaction.set_metadata("method", request_method) if request_method
-        end
-      end
+        request_id = request.env["action_dispatch.request_id"]
+        transaction.set_tags(:request_id => request_id) if request_id
 
-      def fetch_request_id(env)
-        env["action_dispatch.request_id"]
-      end
-
-      def fetch_params(request)
-        return unless request.respond_to?(:filtered_parameters)
-
-        request.filtered_parameters
-      rescue => error
-        # Getting params from the request has been know to fail.
-        Appsignal.internal_logger.debug "Exception while getting Rails params: #{error}"
-        nil
-      end
-
-      def fetch_request_method(request)
-        request.request_method
-      rescue => error
-        Appsignal.internal_logger.error("Unable to report HTTP request method: '#{error}'")
-        nil
+        super
       end
     end
   end
