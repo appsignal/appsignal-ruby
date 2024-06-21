2478eb19f51c18433785347d02af18f405eeeabd
Tom de Bruijn <tom@tomdebruijn.com>
2024-06-21 13:28:56 +0200

    Fix Sinatra instrumentation causing boot loop (#1105)
    I noticed in our test setup that when I added a Sinatra app to the Rails
    app, and loaded the Sinatra instrumentation as specified in our docs it
    would get stuck in a boot loop. This was caused by the two different
    configs overwriting each other every time.
    
    Prevent this issue by skipping loading the AppSignal config if it's
    already active.

--- lib/appsignal/integrations/sinatra.rb
+++ lib/appsignal/integrations/sinatra.rb
@@ -5,13 +5,15 @@ require "appsignal/rack/sinatra_instrumentation"
 
 Appsignal.internal_logger.debug("Loading Sinatra (#{Sinatra::VERSION}) integration")
 
-app_settings = ::Sinatra::Application.settings
-Appsignal.config = Appsignal::Config.new(
-  app_settings.root || Dir.pwd,
-  app_settings.environment
-)
+unless Appsignal.active?
+  app_settings = ::Sinatra::Application.settings
+  Appsignal.config = Appsignal::Config.new(
+    app_settings.root || Dir.pwd,
+    app_settings.environment
+  )
 
-Appsignal.start_logger
-Appsignal.start
+  Appsignal.start_logger
+  Appsignal.start
+end
 
 ::Sinatra::Base.use(Appsignal::Rack::SinatraBaseInstrumentation) if Appsignal.active?
