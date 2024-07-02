57d6fa3386d9a9720da76c7b899a332952d472e0
Tom de Bruijn <tom@tomdebruijn.com>
2024-07-02 17:35:01 +0200

    Deprecate StreamingListener middleware
    Deprecate the StreamingListener now that it is almost identical to the
    AbstractMiddleware. Those couple differences are actually ones we don't
    want people to rely on, so deprecate it.

--- spec/lib/appsignal/rack/streaming_listener_spec.rb
+++ spec/lib/appsignal/rack/streaming_listener_spec.rb
@@ -1,33 +1,69 @@
-require "appsignal/rack/streaming_listener"
-
-describe Appsignal::Rack::StreamingListener do
-  let(:env) { {} }
-  let(:app) { DummyApp.new }
-  let(:middleware) { described_class.new(app, {}) }
-  before { start_agent }
-  around { |example| keep_transactions { example.run } }
-
-  def make_request
-    middleware.call(env)
+describe "Appsignal::Rack::StreamingListener" do
+  def load_middleware
+    load "lib/appsignal/rack/streaming_listener.rb"
   end
 
-  it "instruments the call" do
-    make_request
+  describe "loading the streaming_listener integrations file" do
+    let(:err_stream) { std_stream }
+    let(:stderr) { err_stream.read }
+    after { Appsignal::Rack.send(:remove_const, :StreamingListener) }
 
-    expect(last_transaction).to include_event("name" => "process_streaming_request.rack")
+    it "prints a deprecation warning to STDERR" do
+      capture_std_streams(std_stream, err_stream) do
+        load_middleware
+      end
+
+      expect(stderr).to include(
+        "appsignal WARNING: The constant Appsignal::Rack::StreamingListener " \
+          "has been deprecated."
+      )
+    end
+
+    it "logs a warning" do
+      logs =
+        capture_logs do
+          silence do
+            load_middleware
+          end
+        end
+
+      expect(logs).to contains_log(
+        :warn,
+        "The constant Appsignal::Rack::StreamingListener has been deprecated."
+      )
+    end
   end
 
-  it "set no action by default" do
-    make_request
+  describe "middleware" do
+    let(:env) { {} }
+    let(:app) { DummyApp.new }
+    let(:middleware) { Appsignal::Rack::StreamingListener.new(app, {}) }
+    around { |example| keep_transactions { example.run } }
+    before(:context) { load_middleware }
+    before { start_agent }
 
-    expect(last_transaction).to_not have_action
-  end
+    def make_request
+      middleware.call(env)
+    end
 
-  it "set `appsignal.action` to the action name" do
-    env["appsignal.action"] = "Action"
+    it "instruments the call" do
+      make_request
 
-    make_request
+      expect(last_transaction).to include_event("name" => "process_streaming_request.rack")
+    end
 
-    expect(last_transaction).to have_action("Action")
+    it "set no action by default" do
+      make_request
+
+      expect(last_transaction).to_not have_action
+    end
+
+    it "set `appsignal.action` to the action name" do
+      env["appsignal.action"] = "Action"
+
+      make_request
+
+      expect(last_transaction).to have_action("Action")
+    end
   end
 end
