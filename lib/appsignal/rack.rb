ecf42f137d80f7fc1359ec6dde1d8910fea83cfc
Tom de Bruijn <tom@tomdebruijn.com>
2024-07-04 09:01:40 +0200

    Improve deprecation message for middleware
    Explain a little bit the differences between middlewares and link to the
    docs.

--- lib/appsignal/rack.rb
+++ lib/appsignal/rack.rb
@@ -13,7 +13,12 @@ module Appsignal
         callers = caller
         Appsignal::Utils::StdoutAndLoggerMessage.warning \
           "The constant Appsignal::Rack::GenericInstrumentation has been deprecated. " \
-            "Please update the constant name to " \
+            "Please use the new Appsignal::Rack::InstrumentationMiddleware middleware. " \
+            "This new middleware does not default the action name to 'unknown'. " \
+            "Set the action name for the endpoint using the Appsignal.set_action helper. " \
+            "Read our Rack docs for more information " \
+            "https://docs.appsignal.com/ruby/integrations/rack.html " \
+            "Update the constant name to " \
             "Appsignal::Rack::InstrumentationMiddleware in the following file to " \
             "remove this message.\n#{callers.first}"
         # Return the alias so it can't ever get stuck in a recursive loop
