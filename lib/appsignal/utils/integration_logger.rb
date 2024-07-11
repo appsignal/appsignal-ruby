1b5bbe54dfec0c1fc5e9ad071f3b49d74689ce2c
Tom de Bruijn <tom@tomdebruijn.com>
2024-07-11 13:21:40 +0200

    Remove the warn_once_then_debug from the logger (#1185)
    This method was originally added to stop logging many messages in Grape
    apps nested in Rails apps. That instrumentation didn't work well in
    older versions of the Ruby gem. That's been fixed with the recent Rack
    middleware refactors.
    
    Let's not treat this warning as an annoyance anymore and instead treat
    it as a symptom of an issue we should fix. It should be happening
    anymore in nested apps and is probably an indication of incorrectly
    setup instrumentation.
    
    See issue #584
    
    [skip changeset] because it's an internal refactor

--- lib/appsignal/utils/integration_logger.rb
+++ lib/appsignal/utils/integration_logger.rb
@@ -2,20 +2,7 @@
 
 module Appsignal
   module Utils
-    # Subclass of logger with method to only log a warning once
-    # prevents the local log from filling up with repeated messages.
     class IntegrationLogger < ::Logger
-      def seen_keys
-        @seen_keys ||= Set.new
-      end
-
-      def warn_once_then_debug(key, message)
-        if seen_keys.add?(key).nil?
-          debug message
-        else
-          warn message
-        end
-      end
     end
   end
 end
