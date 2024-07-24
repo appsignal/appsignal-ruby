4d6add1d92255b8e1e6c8187f70258477dc05027
Tom de Bruijn <tom@tomdebruijn.com>
2024-07-24 16:37:57 +0200

    Report Rails runner errors (#1205)

---
+++ .changesets/report-errors-from-rails-runners.md
@@ -0,0 +1,6 @@
+---
+bump: minor
+type: add
+---
+
+Report errors from Rails runners. When a Rails runner reports an unhandled error, it will now report the error in the "runner" namespace.
--- spec/support/helpers/dependency_helper.rb
+++ spec/support/helpers/dependency_helper.rb
@@ -41,6 +41,10 @@ module DependencyHelper
     rails_present? && rails_version >= Gem::Version.new("7.0.0")
   end
 
+  def rails7_1_present?
+    rails_present? && rails_version >= Gem::Version.new("7.1.0")
+  end
+
   def active_job_wraps_args?
     rails7_present? || (ruby_3_1_or_newer? && rails6_1_present? && !rails6_1_5_present?)
   end
