a99bdc75304beef812bdcdf797503c00ca015340
Tom de Bruijn <tom@tomdebruijn.com>
2024-08-08 22:30:30 +0200

    Clean up Demo CLI spec
    - Remove duplicate spec.
    - Remove config setup that's unused.

--- spec/lib/appsignal/cli/demo_spec.rb
+++ spec/lib/appsignal/cli/demo_spec.rb
@@ -7,12 +7,6 @@ describe Appsignal::CLI::Demo do
   let(:out_stream) { std_stream }
   let(:output) { out_stream.read }
   before(:context) { Appsignal.stop }
-  before do
-    ENV.delete("APPSIGNAL_APP_ENV")
-    ENV.delete("RAILS_ENV")
-    ENV.delete("RACK_ENV")
-    stub_api_request config, "auth"
-  end
 
   def run
     run_within_dir project_fixture_path
@@ -25,8 +19,6 @@ describe Appsignal::CLI::Demo do
   end
 
   context "without configuration" do
-    let(:config) { Appsignal::Config.new("development", tmp_dir) }
-
     it "returns an error" do
       expect { run_within_dir tmp_dir }.to raise_error(SystemExit)
 
@@ -35,32 +27,20 @@ describe Appsignal::CLI::Demo do
   end
 
   context "with configuration" do
-    let(:config) { project_fixture_config }
     before do
       # Ignore sleeps to speed up the test
       allow(Appsignal::Demo).to receive(:sleep)
     end
+    let(:options) { { :environment => "development" } }
 
-    context "without environment" do
-      it "returns an error" do
-        expect { run_within_dir tmp_dir }.to raise_error(SystemExit)
-
-        expect(output).to include("Error: Unable to start the AppSignal agent")
-      end
+    it "calls Appsignal::Demo transmitter" do
+      expect(Appsignal::Demo).to receive(:transmit).and_return(true)
+      run
     end
 
-    context "with environment" do
-      let(:options) { { :environment => "development" } }
-
-      it "calls Appsignal::Demo transmitter" do
-        expect(Appsignal::Demo).to receive(:transmit).and_return(true)
-        run
-      end
-
-      it "outputs message" do
-        run
-        expect(output).to include("Demonstration sample data sent!")
-      end
+    it "outputs message" do
+      run
+      expect(output).to include("Demonstration sample data sent!")
     end
   end
 end
