3b1a5b8a86b8ac65196a57d36e6a43467c178397
Tom de Bruijn <tom@tomdebruijn.com>
2024-10-03 16:44:37 +0200

    Fix session data for Action Cable callbacks
    In issue #1313 it was reported that session data was not reported for
    Action Cable callbacks. This broke after PR #1209 where we changed the
    way we handle sample data by allowing merging, which required more
    strict type checks.
    
    By calling `to_h`, ensure the value is a Hash, which is a valid sample
    data type.
    
    Fixes #1313

--- spec/support/helpers/env_helpers.rb
+++ spec/support/helpers/env_helpers.rb
@@ -38,4 +38,32 @@ module EnvHelpers
       :queue_start => fixed_time
     }.merge(args)
   end
+
+  def set_rails_session_data(request, data)
+    ActionDispatch::Request::Session.create(
+      rails_session_store(data),
+      request,
+      {}
+    )
+  end
+
+  def rails_session_store(data)
+    Class.new do
+      def initialize(data)
+        @data = data
+      end
+
+      def load_session(_env)
+        [1, @data]
+      end
+
+      def session_exists?(_env)
+        true
+      end
+
+      def delete_session(_env, _id, _options)
+        123
+      end
+    end.new(data)
+  end
 end
