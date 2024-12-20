34bb293279362014947555a5fd04c7726c537148
Noemi <45180344+unflxw@users.noreply.github.com>
2024-12-20 13:39:46 +0100

    Publish package v4.3.0
    Update version number and CHANGELOG.md.

--- .changesets/fix---silence--implementation-for-logger.md
+++
@@ -1,6 +0,0 @@
----
-bump: patch
-type: fix
----
-
-Fix `#silence` implementation for `Appsignal::Logger`.
--- lib/appsignal/version.rb
+++ lib/appsignal/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Appsignal
-  VERSION = "4.2.3"
+  VERSION = "4.3.0"
 end
