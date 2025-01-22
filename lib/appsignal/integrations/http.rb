fce0acdfe0a7f807c846c3f90fd79f994c44db0b
Noemi <45180344+unflxw@users.noreply.github.com>
2025-01-22 07:44:12 +0100

    Use `HTTP::URI` if defined
    The `http` gem exposes the `HTTP::URI` module, which on different
    versions of the gem either subclasses or extends `Addressable::URI`.
    
    Use it instead of referring directly to `Addressable::URI`, avoiding
    explicitly depending on the implementation detail that `HTTP::URI`
    uses `Addressable::URI` internally.
    
    When `HTTP::URI` is not defined (in `http` gem versions before 0.8.0)
    fall back to using `URI` instead.

--- lib/appsignal/integrations/http.rb
+++ lib/appsignal/integrations/http.rb
@@ -5,7 +5,8 @@ module Appsignal
     # @api private
     module HttpIntegration
       def request(verb, uri, opts = {})
-        parsed_request_uri = uri.is_a?(URI) ? uri : Addressable::URI.parse(uri.to_s)
+        uri_module = defined?(HTTP::URI) ? HTTP::URI : URI
+        parsed_request_uri = uri.is_a?(URI) ? uri : uri_module.parse(uri.to_s)
         request_uri = "#{parsed_request_uri.scheme}://#{parsed_request_uri.host}"
 
         Appsignal.instrument("request.http_rb", "#{verb.upcase} #{request_uri}") do
