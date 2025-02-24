2a6941967435d6672c122e0aa07ec9c2de3e30bc
Tom de Bruijn <tom@tomdebruijn.com>
2025-02-24 15:33:02 +0100

    Fix Capistrano 2 and 3 appsignal.rb config method
    When using an `appsignal.rb` config file, we wouldn't pick up the config
    from the config file.
    
    This changes fixes that by relying on the private `_load_config!` helper
    instead of initializing a Config class directly.

--- lib/appsignal/integrations/capistrano/capistrano_2_tasks.rb
+++ lib/appsignal/integrations/capistrano/capistrano_2_tasks.rb
@@ -5,40 +5,37 @@ module Appsignal
     # @api private
     class Capistrano
       def self.tasks(config)
-        config.load do # rubocop:disable Metrics/BlockLength
+        config.load do
           after "deploy", "appsignal:deploy"
           after "deploy:migrations", "appsignal:deploy"
 
           namespace :appsignal do
             task :deploy do
-              env = fetch(:appsignal_env,
+              appsignal_env = fetch(:appsignal_env,
                 fetch(:stage, fetch(:rails_env, fetch(:rack_env, "production"))))
               user = fetch(:appsignal_user, ENV["USER"] || ENV.fetch("USERNAME", nil))
               revision = fetch(:appsignal_revision, fetch(:current_revision))
 
-              appsignal_config = Appsignal::Config.new(
-                ENV.fetch("PWD", nil),
-                env
-              ).tap do |c|
-                c.merge_dsl_options(fetch(:appsignal_config, {}))
-                c.validate
+              Appsignal._load_config!(appsignal_env) do |conf|
+                conf&.merge_dsl_options(fetch(:appsignal_config, {}))
               end
               Appsignal._start_logger
 
-              if appsignal_config&.active?
+              if Appsignal.config&.active?
                 marker_data = {
                   :revision => revision,
                   :user => user
                 }
 
-                marker = Marker.new(marker_data, appsignal_config)
+                marker = Marker.new(marker_data, Appsignal.config)
                 if config.dry_run
                   puts "Dry run: AppSignal deploy marker not actually sent."
                 else
                   marker.transmit
                 end
               else
-                puts "Not notifying of deploy, config is not active for environment: #{env}"
+                puts "Not notifying of deploy, config is not active for " \
+                  "environment: #{appsignal_env}"
               end
             end
           end
--- spec/lib/appsignal/capistrano2_spec.rb
+++ spec/lib/appsignal/capistrano2_spec.rb
@@ -47,6 +47,174 @@ if DependencyHelper.capistrano2_present?
         ENV["USER"] = "batman"
       end
 
+      context "with appsignal.rb config file" do
+        def write_appsignal_rb_config_file(config_contents)
+          test_path = File.join(tmp_dir, "config_file_test_#{SecureRandom.uuid}")
+          FileUtils.mkdir_p(test_path)
+          Dir.chdir(test_path) do
+            write_file(File.join(test_path, "config", "appsignal.rb"), config_contents)
+          end
+          test_path
+        end
+
+        around do |example|
+          tmp_config_dir = write_appsignal_rb_config_file(
+            <<~CONFIG
+              Appsignal.configure do |config|
+                config.active = true
+                config.name = "CapDSLapp"
+                config.push_api_key = "cap_push_api_key"
+              end
+            CONFIG
+          )
+          Dir.chdir(tmp_config_dir) { example.run }
+        end
+
+        it "reads the base config from the config file" do
+          marker_request = stub_marker_request(
+            {
+              :environment => "production",
+              :name => "CapDSLapp",
+              :push_api_key => "cap_push_api_key"
+            },
+            marker_data
+          ).to_return(:status => 200)
+
+          run
+
+          expect(marker_request).to have_been_made
+        end
+
+        context "when rack_env is the only env set" do
+          let(:env) { "rack_production" }
+          before do
+            capistrano_config.unset(:rails_env)
+            capistrano_config.set(:rack_env, env)
+          end
+
+          it "uses the rack_env as the env" do
+            marker_request = stub_marker_request(
+              {
+                :environment => env.to_s,
+                :name => "CapDSLapp",
+                :push_api_key => "cap_push_api_key"
+              },
+              marker_data
+            ).to_return(:status => 200)
+
+            run
+
+            expect(marker_request).to have_been_made
+          end
+        end
+
+        context "when stage is set" do
+          let(:env) { "stage_production" }
+          before do
+            capistrano_config.set(:rack_env, "rack_production")
+            capistrano_config.set(:stage, env)
+          end
+
+          it "prefers the Capistrano stage rather than rails_env and rack_env" do
+            marker_request = stub_marker_request(
+              {
+                :environment => env.to_s,
+                :name => "CapDSLapp",
+                :push_api_key => "cap_push_api_key"
+              },
+              marker_data
+            ).to_return(:status => 200)
+
+            run
+
+            expect(marker_request).to have_been_made
+          end
+        end
+
+        context "when `appsignal_config` is set" do
+          before do
+            ENV["APPSIGNAL_APP_NAME"] = "EnvName"
+            capistrano_config.set(:appsignal_config, :name => "CapName")
+          end
+
+          it "overrides the default config with the custom appsignal_config" do
+            marker_request = stub_marker_request(
+              {
+                :environment => env.to_s,
+                :name => "CapName",
+                :push_api_key => "cap_push_api_key"
+              },
+              marker_data
+            ).to_return(:status => 200)
+
+            run
+
+            expect(marker_request).to have_been_made
+          end
+
+          context "with invalid config" do
+            before do
+              capistrano_config.set(:appsignal_config, :push_api_key => nil)
+            end
+
+            it "does not continue with invalid config" do
+              run
+              expect(output).to include \
+                "Not notifying of deploy, config is not active for environment: production"
+              expect(logs).to contains_log(:error, "Push API key not set after loading config")
+            end
+          end
+        end
+
+        context "when `appsignal_env` is set as a string" do
+          let(:env) { "appsignal_production" }
+          before do
+            capistrano_config.set(:rack_env, "rack_production")
+            capistrano_config.set(:stage, "stage_production")
+            capistrano_config.set(:appsignal_env, env)
+          end
+
+          it "prefers the appsignal_env rather than stage, rails_env and rack_env" do
+            marker_request = stub_marker_request(
+              {
+                :environment => env.to_s,
+                :name => "CapDSLapp",
+                :push_api_key => "cap_push_api_key"
+              },
+              marker_data
+            ).to_return(:status => 200)
+
+            run
+
+            expect(marker_request).to have_been_made
+          end
+        end
+
+        context "when `appsignal_env` is set as a symbol" do
+          let(:env) { :appsignal_production }
+          before do
+            capistrano_config.set(:rack_env, "rack_production")
+            capistrano_config.set(:stage, "stage_production")
+            capistrano_config.set(:appsignal_env, env)
+          end
+
+          it "prefers the appsignal_env rather than stage, rails_env and rack_env" do
+            marker_request = stub_marker_request(
+              {
+                :environment => env.to_s,
+                :name => "CapDSLapp",
+                :push_api_key => "cap_push_api_key"
+              },
+              marker_data
+            ).to_return(:status => 200)
+
+            run
+
+            expect(marker_request).to have_been_made
+          end
+        end
+      end
+
       context "with appsignal.yml config file" do
         around do |example|
           Dir.chdir project_fixture_path do
