01c6dd5b807844b6b0b0352fa098467f80bf4386
Martin Lipskis <martins.lipskis@gmail.com>
2025-09-17 18:59:49 +0300

    Handle falsy values and simplify tests
    This change properly handles falsy values from
    `CodeOwnership.for_backtrace`.
    
    The change adds tests for internal error logging when config isn't set
    up correctly, swaps `require` for `load` and simplifies the integration
    spec.

--- lib/appsignal/integrations/code_ownership.rb
+++ lib/appsignal/integrations/code_ownership.rb
@@ -6,20 +6,14 @@ module Appsignal
     module CodeOwnershipIntegration
       class << self
         def before_complete(transaction, error)
-          team = nil
-          begin
-            team = ::CodeOwnership.for_backtrace(error.backtrace)
-          rescue => ex
-            logger = Appsignal.internal_logger
-            logger.error(
-              "Error while looking up CodeOwnership team: #{ex.class}: #{ex.message}\n" \
-                "#{ex.backtrace.join("\n")}"
-            )
-          end
-
-          return if team.nil?
-
+          team = ::CodeOwnership.for_backtrace(error.backtrace)
           transaction.add_tags(:owner => team.name)
+        rescue => ex
+          logger = Appsignal.internal_logger
+          logger.error(
+            "Error while looking up CodeOwnership team: #{ex.class}: #{ex.message}\n" \
+              "#{ex.backtrace.join("\n")}"
+          )
         end
       end
     end
--- spec/lib/appsignal/integrations/code_ownership_spec.rb
+++ spec/lib/appsignal/integrations/code_ownership_spec.rb
@@ -2,47 +2,13 @@ if DependencyHelper.code_ownership_present?
   require "appsignal/integrations/code_ownership"
 
   describe Appsignal::Integrations::CodeOwnershipIntegration do
-    def create_config_file
-      yaml_config =
-        <<~CONFIG
-          owned_globs:
-            - '{app,config}/**/*.{rb,rake,js,jsx,ts,tsx}'
-          js_package_paths: []
-        CONFIG
-
-      write_file(File.join(tmp_dir, "config", "code_ownership.yml"), yaml_config)
-    end
-
-    def create_team_files
-      file_team =
-        <<~CONFIG
-          name: FileTeam
-        CONFIG
-
-      write_file(File.join(tmp_dir, "config", "teams", "file.yml"), file_team)
-
-      dir_team =
-        <<~CONFIG
-          name: DirectoryTeam
-        CONFIG
-
-      write_file(File.join(tmp_dir, "config", "teams", "directory.yml"), dir_team)
-
-      glob_team =
-        <<~CONFIG
-          name: GlobTeam
-          owned_globs:
-            - #{File.join(support_dir, "code_ownership", "glob", "*.rb")}
-        CONFIG
-
-      write_file(File.join(tmp_dir, "config", "teams", "glob.yml"), glob_team)
-    end
-
     before do
       start_agent
 
       Appsignal::Hooks::CodeOwnershipHook.new.install
     end
+
+    around { |example| keep_transactions { example.run } }
 
     context "when an error is reported in a transaction" do
       after do
@@ -52,26 +18,38 @@ if DependencyHelper.code_ownership_present?
       it "handles missing config file" do
         transaction = create_transaction
 
-        begin
-          require File.join(support_dir, "code_ownership", "file_annotation_based.rb")
+        logs = capture_logs do
+          load File.join(support_dir, "code_ownership", "file_annotation_based.rb")
         rescue => error
           transaction.add_error(error)
         ensure
-          expect { keep_transactions { transaction.complete } }.not_to raise_error
+          transaction.complete
         end
+
+        expect(transaction).to_not include_tags("owner" => anything)
+        expect(logs).to contains_log(
+          :error,
+          "Error while looking up CodeOwnership team"
+        )
       end
 
       it "handles missing team config files" do
         create_config_file
         transaction = create_transaction
 
-        begin
-          require File.join(support_dir, "code_ownership", "file_annotation_based.rb")
+        logs = capture_logs do
+          load File.join(support_dir, "code_ownership", "file_annotation_based.rb")
         rescue => error
           transaction.add_error(error)
         ensure
-          expect { keep_transactions { transaction.complete } }.not_to raise_error
+          transaction.complete
         end
+
+        expect(transaction).to_not include_tags("owner" => anything)
+        expect(logs).to contains_log(
+          :error,
+          "Error while looking up CodeOwnership team"
+        )
       end
 
       context "and config is set up correctly" do
@@ -94,11 +72,11 @@ if DependencyHelper.code_ownership_present?
           transaction = create_transaction
 
           begin
-            require File.join(support_dir, "code_ownership", "file_annotation_based.rb")
+            load File.join(support_dir, "code_ownership", "file_annotation_based.rb")
           rescue => error
             transaction.add_error(error)
           ensure
-            keep_transactions { transaction.complete }
+            transaction.complete
           end
 
           expect(transaction).to include_tags("owner" => "FileTeam")
@@ -108,11 +86,11 @@ if DependencyHelper.code_ownership_present?
           transaction = create_transaction
 
           begin
-            require File.join(support_dir, "code_ownership", "dir", "directory_based.rb")
+            load File.join(support_dir, "code_ownership", "dir", "directory_based.rb")
           rescue => error
             transaction.add_error(error)
           ensure
-            keep_transactions { transaction.complete }
+            transaction.complete
           end
 
           expect(transaction).to include_tags("owner" => "DirectoryTeam")
@@ -122,16 +100,46 @@ if DependencyHelper.code_ownership_present?
           transaction = create_transaction
 
           begin
-            require File.join(support_dir, "code_ownership", "glob", "glob_based.rb")
+            load File.join(support_dir, "code_ownership", "glob", "glob_based.rb")
           rescue => error
             transaction.add_error(error)
           ensure
-            keep_transactions { transaction.complete }
+            transaction.complete
           end
 
           expect(transaction).to include_tags("owner" => "GlobTeam")
         end
       end
+    end
+
+    private
+
+    def create_config_file
+      FileUtils.mkdir(File.join(tmp_dir, "config"))
+      FileUtils.copy_file(
+        File.join(support_dir, "code_ownership", "config", "code_ownership.yml"),
+        File.join(tmp_dir, "config", "code_ownership.yml")
+      )
+    end
+
+    def create_team_files
+      FileUtils.mkdir(File.join(tmp_dir, "config", "teams"))
+
+      %w[file directory].each do |team|
+        FileUtils.copy_file(
+          File.join(support_dir, "code_ownership", "config", "teams", "#{team}.yml"),
+          File.join(tmp_dir, "config", "teams", "#{team}.yml")
+        )
+      end
+
+      glob_team =
+        <<~CONFIG
+          name: GlobTeam
+          owned_globs:
+            - #{File.join(support_dir, "code_ownership", "glob", "*.rb")}
+        CONFIG
+
+      write_file(File.join(tmp_dir, "config", "teams", "glob.yml"), glob_team)
     end
   end
 end
