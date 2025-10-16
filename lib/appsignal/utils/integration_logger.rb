5374088f4fb82466227b741563dc5ec9c8788a78
Tom de Bruijn <tom@tomdebruijn.com>
2025-10-16 16:00:15 +0200

    Truncate internal log lines
    Truncate logs sent to our internal integrations logger.
    
    We had a report that for a `ActionController::BadRequest` error the
    appsignal.log file filled up with the uploaded file, as it's part of the
    error message.
    This will limit the log line length to 2000 characters. We can
    increase this if needed.
    
    Closes https://github.com/appsignal/support/issues/364

--- lib/appsignal/utils/integration_logger.rb
+++ lib/appsignal/utils/integration_logger.rb
@@ -3,6 +3,31 @@
 module Appsignal
   module Utils
     class IntegrationLogger < ::Logger
+      MAX_MESSAGE_LENGTH = 2_000
+
+      def add(severity, message = nil, progname = nil)
+        if message.nil? && !block_given?
+          # When called as logger.error("msg"), the message is in progname
+          progname = truncate_message(progname)
+        elsif message
+          message = truncate_message(message)
+        elsif block_given?
+          message = truncate_message(yield)
+        end
+        super
+      end
+
+      private
+
+      def truncate_message(message)
+        return message unless message.is_a?(String)
+
+        if message.length > MAX_MESSAGE_LENGTH
+          "#{message[0, MAX_MESSAGE_LENGTH]}..."
+        else
+          message
+        end
+      end
     end
   end
 end
