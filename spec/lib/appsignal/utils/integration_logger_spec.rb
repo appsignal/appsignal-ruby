58401e31fd81dc1fc24c5bafe104a936cf646667
Tom de Bruijn <tom@tomdebruijn.com>
2025-10-17 14:59:51 +0200

    Truncate internal log lines (#1469)
    * Truncate internal log lines
    
    Truncate logs sent to our internal integrations logger.
    
    We had a report that for a `ActionController::BadRequest` error the
    appsignal.log file filled up with the uploaded file, as it's part of the
    error message.
    This will limit the log line length to 2000 characters. We can
    increase this if needed.
    
    Closes https://github.com/appsignal/support/issues/364
    
    * Fix typo in changeset
    
    Change truncate to truncated in the changeset.
    
    [skip ci]
    
    Co-authored-by: Martin Lipskis <lipskis@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Martin Lipskis <lipskis@users.noreply.github.com>

--- spec/lib/appsignal/utils/integration_logger_spec.rb
+++ spec/lib/appsignal/utils/integration_logger_spec.rb
@@ -12,10 +12,58 @@ describe Appsignal::Utils::IntegrationLogger do
     logger.info("info message")
     logger.warn("warning message")
     logger.error("error message")
+    logger.error(ExampleStandardError.new("example error with message"))
+    logger.info { "block error message" }
 
     expect(logs).to contains_log(:debug, "debug message")
     expect(logs).to contains_log(:info, "info message")
     expect(logs).to contains_log(:warn, "warning message")
     expect(logs).to contains_log(:error, "error message")
+    expect(logs).to contains_log(:error, "example error with message")
+    expect(logs).to contains_log(:info, "block error message")
+  end
+
+  describe "message truncation" do
+    it "does not truncate short messages" do
+      logger.error("Short error message")
+
+      expect(logs).to contains_log(:error, "Short error message")
+    end
+
+    context "when calling logger.error(message)" do
+      it "truncates long messages" do
+        long_message = "a" * 2500
+        logger.error(long_message)
+
+        expect(logs).to contains_log(:error, "#{"a" * 2000}...")
+      end
+    end
+
+    context "when calling logger.error { message }" do
+      it "truncates long messages passed as a block" do
+        long_message = "a" * 2500
+        logger.error { long_message }
+
+        expect(logs).to contains_log(:error, "#{"a" * 2000}...")
+      end
+    end
+
+    context "when calling logger.add(severity, message)" do
+      it "truncates long messages" do
+        long_message = "a" * 2500
+        logger.add(Logger::ERROR, long_message)
+
+        expect(logs).to contains_log(:error, "#{"a" * 2000}...")
+      end
+    end
+
+    context "when calling logger.error(progname) { message }" do
+      it "truncates long messages from block" do
+        long_message = "a" * 2500
+        logger.error("progname") { long_message }
+
+        expect(logs).to contains_log(:error, "#{"a" * 2000}...")
+      end
+    end
   end
 end
