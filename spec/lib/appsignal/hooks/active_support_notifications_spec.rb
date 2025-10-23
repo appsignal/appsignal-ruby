242d9e21752688b153534d93965b19651f6d7a40
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-10-23 19:30:52 +0200

    Don't run start & finish examples when Fanout::NullHandle exists
    With ActiveSupport::Notifications::Fanout::NullHandle now existing, we
    have a state where ActiveSupport::Notifications::Fanout::Handle
    exists, but the start_finish_shared_examples will fail because the
    NullHandleAwareInstrumentIntegration uses the #instrument integration
    we use when Fanout::Handle doesn't exist.
    
    Therefor, when Fanout::NullHandle exists, we can rely on the main
    tests and don't have to include the start_finish ones.

--- spec/lib/appsignal/hooks/active_support_notifications_spec.rb
+++ spec/lib/appsignal/hooks/active_support_notifications_spec.rb
@@ -20,22 +20,24 @@ describe Appsignal::Hooks::ActiveSupportNotificationsHook do
 
     it_behaves_like "activesupport instrument override"
 
-    if defined?(::ActiveSupport::Notifications::Fanout::Handle)
-      require_relative "active_support_notifications/start_finish_shared_examples"
+    unless defined?(::ActiveSupport::Notifications::Fanout::NullHandle)
+      if defined?(::ActiveSupport::Notifications::Fanout::Handle)
+        require_relative "active_support_notifications/start_finish_shared_examples"
 
-      it_behaves_like "activesupport start finish override"
-    end
+        it_behaves_like "activesupport start finish override"
+      end
 
-    if ::ActiveSupport::Notifications::Instrumenter.method_defined?(:start)
-      require_relative "active_support_notifications/start_finish_shared_examples"
+      if ::ActiveSupport::Notifications::Instrumenter.method_defined?(:start)
+        require_relative "active_support_notifications/start_finish_shared_examples"
 
-      it_behaves_like "activesupport start finish override"
-    end
+        it_behaves_like "activesupport start finish override"
+      end
 
-    if ::ActiveSupport::Notifications::Instrumenter.method_defined?(:finish_with_state)
-      require_relative "active_support_notifications/finish_with_state_shared_examples"
+      if ::ActiveSupport::Notifications::Instrumenter.method_defined?(:finish_with_state)
+        require_relative "active_support_notifications/finish_with_state_shared_examples"
 
-      it_behaves_like "activesupport finish_with_state override"
+        it_behaves_like "activesupport finish_with_state override"
+      end
     end
   else
     describe "#dependencies_present?" do
